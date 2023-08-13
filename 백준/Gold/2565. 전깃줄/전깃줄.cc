756f0d678dabd67628c650cd80bbaae5b382811c
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-08-13 16:49:08 +0900

    [Gold V] Title: 전깃줄, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[501]{}, DP[501]{};
+int Size;
+
+void Input() {
+	cin >> Size;
+
+	for (int i = 0; i < Size; i++) {
+		int index; cin >> index;
+		cin >> arr[index];
+		DP[index] = 1;
+	}
+}
+
+void Function() {
+	for (int i = 1; i < 501; i++)
+		for (int j = 1; j < i; j++)
+			if (arr[i] > arr[j] && arr[i] != 0)
+				DP[i] = max(DP[i], DP[j] + 1);
+}
+
+void PrintResult() {
+	int Max = DP[1];
+	for (int i = 2; i < 501; i++)
+		Max = max(Max, DP[i]);
+
+	cout << Size - Max;
+}
+
+int main() {
+	Input();
+	Function();
+	PrintResult();
+}
