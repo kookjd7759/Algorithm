6aaf27ae1d3a576f6340545dd631e99749bf45a1
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-08-19 16:40:09 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 124 ms, Memory: 22908 KB -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int Arr[26][200000];
+
+void SetArr(string st) {
+	int index = (int)st[0] - 97;
+	Arr[index][0] = 1;
+	for (int i = 1; i < st.length(); i++) {
+		int index = (int)st[i] - 97;
+		for (int j = 0; j < 26; j++) {
+			Arr[j][i] = Arr[j][i - 1];
+			if (j == index)
+				Arr[j][i]++;
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	string st; cin >> st;
+	SetArr(st);
+	/*
+	for (int i = 0; i < 26; i++) {
+		cout << (char)(97 + i) << " : ";
+		for (int j = 0; j < st.length(); j++) {
+			cout << Arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	*/
+	int size; cin >> size;
+	while (size--) {
+		string temp; cin >> temp;
+		int start, end; cin >> start >> end;
+		int index = (int)(temp[0] - 97);
+		if (start == 0)
+			cout << Arr[index][end] << "\n";
+		else
+			cout << Arr[index][end] - Arr[index][start - 1] << "\n";
+	}
+}
