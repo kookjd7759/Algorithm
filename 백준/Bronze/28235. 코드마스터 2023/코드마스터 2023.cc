8e57690b471aad977967bcc83c672a0e1d736435
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-08-20 19:31:40 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string st; cin >> st;
+	if (st == "SONGDO")
+		cout << "HIGHSCHOOL";
+	else if (st == "CODE")
+		cout << "MASTER";
+	else if (st == "2023")
+		cout << "0611";
+	else if (st == "ALGORITHM")
+		cout << "CONTEST";
+}
