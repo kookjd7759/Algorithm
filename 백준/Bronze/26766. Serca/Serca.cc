d3172ec05fc10207cc355f36f35b3da61ca6b0f1
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-08-20 19:26:55 +0900

    [Bronze V] Title: Serca, Time: 84 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	int num; cin >> num;
+	while (num--) {
+		cout << " @@@   @@@ \n";
+		cout << "@   @ @   @\n";
+		cout << "@    @    @\n";
+		cout << "@         @\n";
+		cout << " @       @ \n";
+		cout << "  @     @  \n";
+		cout << "   @   @   \n";
+		cout << "    @ @    \n";
+		cout << "     @     \n";
+	}
+}
