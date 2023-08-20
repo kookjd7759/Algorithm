0c783ab99d8af0e653b217f46e5c7d3da9ee3b98
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-08-20 19:43:36 +0900

    [Bronze V] Title: 時間 (Hour), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int x; cin >> x;
+
+  int hours = 24 * x;
+
+  cout << hours << "\n";
+
+  return 0;
+}
