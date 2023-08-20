201a130bce1fbaf28496a130cc1d69163c415ed8
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-08-20 19:17:32 +0900

    [Bronze V] Title: Football Scoring, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, c, d, e; 
+	while (cin >> a >> b >> c >> d >> e) {
+		cout << a * 6 + b * 3 + c * 2 + d * 1 + e * 2 << "\n";
+	}
+}
