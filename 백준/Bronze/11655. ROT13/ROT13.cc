0e7c7c1be5d33574f4ccb404168fb7da377b643e
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-09-13 19:18:15 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int dp[1005];
+
+int main() {
+	string st; getline(cin, st);
+	for (int i = 0; i < st.size(); i++) {
+		if (st[i] >= 97 && st[i] <= 122) {
+			st[i] += (13 - 97);
+			st[i] %= 26;
+			st[i] += 97;
+		}
+		else if (st[i] >= 65 && st[i] <= 90) {
+			st[i] += (13 - 65);
+			st[i] %= 26;
+			st[i] += 65;
+		}
+	}
+	cout << st;
+}
