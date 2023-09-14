074d6560fb388e1f278816c23a237c1b5cd2f183
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-09-14 14:15:18 +0900

    [Silver V] Title: 거스름돈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	int n; cin >> n;
+	if (n < 10){
+		if (n == 1 || n == 3)
+			cout << -1;
+		else if (n == 5)
+			cout << 1;
+		else if (n == 2 || n == 4 || n == 6 || n == 8)
+			cout << n / 2;
+		else if (n == 7)
+			cout << 2;
+		else 
+			cout << 3;
+	}
+	else {
+		int num = n % 10;
+		if (num == 0 || num == 5)
+			cout << n / 5;
+		else if (num == 1)
+			cout << 3 + ((n - 6) / 5);
+		else if (num == 2)
+			cout << 1 + ((n - 2) / 5);
+		else if (num == 3)
+			cout << 4 + ((n - 8) / 5);
+		else if (num == 4)
+			cout << 2 + ((n - 4) / 5);
+		else if (num == 6)
+			cout << 3 + ((n - 6) / 5);
+		else if (num == 7)
+			cout << 1 + ((n - 2) / 5);
+		else if (num == 8)
+			cout << 4 + ((n - 8) / 5);
+		else 
+			cout << 2 + ((n - 4) / 5);
+	}		
+}
