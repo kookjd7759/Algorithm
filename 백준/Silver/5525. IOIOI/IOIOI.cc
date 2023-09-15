bf432d1564af2d01d33af38a1f6d603f99715b9d
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-09-15 15:17:35 +0900

    [Silver I] Title: IOIOI, Time: 40 ms, Memory: 4976 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> vec;
+
+int main(){
+	int n, m; cin >> n >> m;
+	string st; cin >> st;
+	
+	int cnt(0);
+	bool check = false;
+	for (int i = 0; i < m - 2; i++){
+		//cout << i << " ";
+		if (st[i] == 'I'){
+			if (st[i + 1] == 'O') {
+				if (st[i + 2] == 'I'){
+					cnt++;
+					i += 1;
+				}
+				else {
+					if (cnt) vec.push_back(cnt);
+					cnt = 0;
+					i += 2;
+				}
+			}
+			else {
+				if (cnt) vec.push_back(cnt);
+				cnt = 0;
+			}
+		}
+		else {
+			if (cnt) vec.push_back(cnt);
+			cnt = 0;
+		}
+	}
+	//cout << "\n";
+
+	if (cnt) vec.push_back(cnt);
+	
+	int ret(0);
+	for (const auto iter : vec){
+		if (iter  >= n)
+			ret += (iter - (n - 1));
+		//cout << iter << " ";
+	}
+	//cout << "\n";
+	cout << ret;
+}
