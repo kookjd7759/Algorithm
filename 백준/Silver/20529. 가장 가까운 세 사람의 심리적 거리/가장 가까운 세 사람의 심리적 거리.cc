2274f0d99406598d051925f930b8c903d9b125f0
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-09-16 16:15:48 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 20 ms, Memory: 5272 KB -BaekjoonHub

--- 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.cc
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.cc
@@ -25,22 +25,14 @@ int main() {
 			cin >> mbti[i];
 
 		int ret(0);
-		string mbti_ret[3];
 		if (n > 32)
 			ret = 0;
 		else {
 			int min_ = 128;
 			for (int i = 0; i < n - 2; i++)
 				for (int j = i + 1; j < n - 1; j++)
-					for (int k = j + 1; k < n; k++) {
-						if (min_ > getScore(mbti[i], mbti[j], mbti[k])) {
-							mbti_ret[0] = mbti[i];
-							mbti_ret[1] = mbti[j];
-							mbti_ret[2] = mbti[k];
-							min_ = getScore(mbti[i], mbti[j], mbti[k]);
-						}
-						// min_ = min(min_, getScore(mbti[i], mbti[j], mbti[k]));
-					}
+					for (int k = j + 1; k < n; k++) 
+						min_ = min(min_, getScore(mbti[i], mbti[j], mbti[k]));
 			ret = min_;
 		}
 		cout << ret << "\n";
