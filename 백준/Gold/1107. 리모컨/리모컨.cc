6b5ed197df17fd6b20c7dce753414c68df1bd65e
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-09-16 18:43:54 +0900

    [Gold V] Title: 리모컨, Time: 52 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.cc
+++ 백준/Gold/1107. 리모컨/리모컨.cc
@@ -1,38 +1,27 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#define MIN(a,b) (a<b ? a : b)
+#include <iostream>
+#include <string>
+
 using namespace std;
 
-int min_cnt = 0x3f3f3f3f, num_int;
-int btn_set[10] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+int cnt = 0x3f3f3f3f, num_int;
+bool btn[10] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
 
-void find(string num) {
-    for(int i=0;i<10;i++) {
-	    if(btn_set[i]) {
-	        string tmp_num = num + to_string(i);
-	        min_cnt = MIN(min_cnt, abs(num_int - stoi(tmp_num)) + tmp_num.length());
+void func(string num) {
+	for (int i = 0; i < 10; i++) {
+		if (btn[i]) {
+			string tmp_num = num + to_string(i);
+			int newNum = abs(num_int - stoi(tmp_num)) + tmp_num.length();
+			if (cnt >= newNum) cnt = newNum;
 
-    	    if(tmp_num.length() < 6) {
-	    	    find(tmp_num);
-	        }
-	    }
-    }
+			if (tmp_num.length() < 6) func(tmp_num);
+		}
+	}
 }
 
 int main() {
-    int n, k;
-    scanf("%d%d", &num_int, &n);
-    min_cnt = MIN(min_cnt, abs(100 - num_int));
-
-    for(int i=0;i<n;i++) {
-    	scanf("%d", &k);
-	    btn_set[k] = 0;
-    }
-
-    if(n<10) {
-	    find("");
-    }
-
-    printf("%d", min_cnt);
+	int n, k; cin >> num_int >> n;
+	cnt = min(cnt, abs(100 - num_int));
+	for (int i = 0; i < n; i++) { cin >> k; btn[k] = 0; }
+	if (n < 10) func("");
+	cout << cnt;
 }
