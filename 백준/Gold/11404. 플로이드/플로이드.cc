59abefee6933c2211daefe556cba480b2fec81db
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-09-18 13:48:37 +0900

    [Gold IV] Title: 플로이드, Time: 28 ms, Memory: 2196 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int n, m;
+int graph[101][101];
+vector<int>* path;
+
+void dijkstra(int start) {
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> que; que.push({ 0, start });
+	path = new vector<int>(n + 1, -1); path->at(start) = 0;
+	
+	while (!que.empty()) {
+		int cur_cost = que.top().first;
+		int cur_node = que.top().second;
+		que.pop();
+		
+		if (path->at(cur_node) != -1 && path->at(cur_node) < cur_cost)
+			continue;
+		
+		for (int i = 1; i <= n; i++) {
+			if (graph[cur_node][i] == -1)
+				continue;
+			
+			int next_cost = cur_cost + graph[cur_node][i];
+			int next_node = i;
+			if (path->at(next_node) == -1 || next_cost < path->at(next_node)) {
+				path->at(next_node) = next_cost;
+				que.push({ next_cost, next_node });
+			}
+		}
+	}
+	
+	for (int i = 1; i <= n; i++){
+		path->at(i) != -1 ? cout << path->at(i) : cout << 0;
+		cout << " ";
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	memset(graph, -1, sizeof(graph));
+	
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		int a, b, c; cin >> a >> b >> c;
+		graph[a][b] != -1 ? graph[a][b] = min(graph[a][b], c) : graph[a][b] = c;
+	}
+	
+	for (int i = 1; i <= n; i++){
+		dijkstra(i);
+		cout << "\n";
+	}
+}
