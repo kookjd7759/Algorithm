d7939d5a6c118eeeb90fa4817b7389d43f04152a
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-09-20 09:33:11 +0900

    [Gold II] Title: 홀수번째 피보나치 수의 합, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/11442. 홀수번째 피보나치 수의 합/홀수번째 피보나치 수의 합.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <map>
+
+#define ll long long
+
+using namespace std;
+
+const ll NUM = 1000000007;
+map<ll, ll> m;
+
+ll F(ll n){
+	if (!n) return 0;
+	if (n == 1 || n == 2) return 1;
+	if (m.count(n)) return m[n];
+	
+	if (n & 1){ // odd
+		ll a = F((n >> 1) + 1);
+		ll b = F(n >> 1);
+		m[n] = ((a * a) % NUM + (b * b) % NUM) % NUM;
+		return m[n];
+	}
+	else { // even
+		ll a = F((n >> 1)- 1);
+		ll b = F(n >> 1);
+		m[n] = ((2 * a + b) % NUM * b % NUM) % NUM;
+		return m[n];
+	}
+}
+
+int main(){
+	ios::sync_with_stdio(0); cin.tie(0);
+	ll n; cin >> n;
+	if (n & 1) n++;
+	cout << F(n);
+}
+	
