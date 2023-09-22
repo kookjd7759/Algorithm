d63cd1d5166683c0a60fd1e41918e52df4249c32
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-09-22 13:58:43 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 224 ms, Memory: 18216 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int n;
+int nList[1000001];
+int DP[1000001];
+vector<int> vec;
+stack<int> st;
+int max_index(1);
+
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int n; cin >> n;
+	for (int i = 1; i <= n; i++) cin >> nList[i];
+	
+	vec.push_back(nList[1]); DP[1] = 1;
+	for (int i = 2; i <= n; i++){
+		if (vec[vec.size() - 1] < nList[i]) {
+			vec.push_back(nList[i]);
+			DP[i] = vec.size();
+			max_index = i;
+		}
+		else {
+			int index = lower_bound(vec.begin(), vec.end(), nList[i]) - vec.begin();
+			vec[index] = nList[i];
+			DP[i] = index + 1;
+		}
+	}
+	
+	st.push(max_index);
+	for (int i = max_index - 1; i > 0; i--){
+		if (DP[i] + 1 == DP[max_index]){
+			max_index = i;
+			st.push(i);
+		}
+	}
+	
+	cout << vec.size() << "\n";
+	
+	while(!st.empty()){
+		cout << nList[st.top()] << " ";
+		st.pop();
+	}
+}
