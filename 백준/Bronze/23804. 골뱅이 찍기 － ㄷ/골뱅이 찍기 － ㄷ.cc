d844922d46244394e2b9d1f209d74e3e24ad17bb
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-09-22 15:04:58 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄷ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23804. 골뱅이 찍기 － ㄷ/골뱅이 찍기 － ㄷ.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(0); cout.tie(0);
+	int n; cin >> n;
+	for (int i = 0; i < n; i++){
+		for (int i = 0; i < n * 5; i++)
+			cout << "@";
+		cout << "\n";
+	}
+	for (int i = 0; i < n * 3; i++){
+		for (int i = 0; i < n; i++)
+			cout << "@";
+		cout << "\n";
+	}
+	for (int i = 0; i < n; i++){
+		for (int i = 0; i < n * 5; i++)
+			cout << "@";
+		cout << "\n";
+	}
+}
