7158931310a735fc4bbd8e34cd8500f21eb734b2
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-09-22 15:12:34 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㄹ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23805. 골뱅이 찍기 － 돌아간 ㄹ/골뱅이 찍기 － 돌아간 ㄹ.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(0); cout.tie(0);
+	auto P_2 = [&](int size) -> void {
+		for (int i = 0; i < size; i++) cout << "@";
+		};
+	auto P_s = [&](int size) -> void {
+		for (int i = 0; i < size; i++) cout << " ";
+		};
+	int n; cin >> n;
+	for (int i = 0; i < n; i++){
+		P_2 (n * 3); P_s (n); P_2 (n);
+		cout << "\n";
+	}
+	for (int i = 0; i < n * 3; i++){
+		P_2 (n); P_s (n); P_2 (n); P_s (n); P_2 (n);
+		cout << "\n";
+	}
+	for (int i = 0; i < n; i++){
+		P_2 (n); P_s (n); P_2(n * 3);
+		cout << "\n";
+	}
+}
