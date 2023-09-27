8300c83b00193e8f830d0913369dbb00e4a3930d
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-09-27 09:33:55 +0900

    [Bronze IV] Title: Do Not Touch Anything, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13136. Do Not Touch Anything/Do Not Touch Anything.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double a, b, c; cin >> a >> b >> c;
+    cout << ((long long)ceil(a / c) * (long long)ceil(b / c));
+}
