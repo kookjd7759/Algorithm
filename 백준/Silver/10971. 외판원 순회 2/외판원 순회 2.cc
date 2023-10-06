c3d0746d4e364bfa9e3e34c700baafe586bf83a2
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-06 16:21:14 +0900

    [Silver II] Title: 외판원 순회 2, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+#define INF 2e9
+#define SIZE 10
+
+using namespace std;
+
+int N;
+int graph[SIZE][SIZE];
+int DP[SIZE][1 << SIZE];
+
+int TSP(int visited, int now){
+	visited |= (1 << now);
+	
+	if (visited == (1 << N) - 1) {
+		if (graph[now][0]) return graph[now][0];
+		return INF;
+	}
+	
+	int& ret = DP[now][visited];
+	if (DP[now][visited] > 0) return ret;
+	
+	ret = INF;
+	for (int i = 0; i < N; i++){
+		if (i != now && (visited & (1 << i)) == 0 && graph[now][i]){
+			int temp = TSP(visited, i) + graph[now][i];
+			if (ret > temp) ret = temp;
+		}
+	}
+	return ret;
+}
+
+int main(void){
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	memset(DP, -1, sizeof(DP));
+
+	cin >> N;
+	for (int i = 0; i < N; i++) for (int j = 0; j < N; j++) 
+		cin >> graph[i][j];
+	
+	cout << TSP(0, 0);
+}
