9aaf0c502c315203407b1f14401fa7abc1fff4e3
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-10 15:02:08 +0900

    [Bronze IV] Title: Judging Moose, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15025. Judging Moose/Judging Moose.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b; cin >> a >> b;
+	if (a == 0 && b == 0) cout << "Not a moose";
+	else if (a == b) cout << "Even " << a + b;
+	else cout << "Odd " << max(a, b) * 2; 
+}
