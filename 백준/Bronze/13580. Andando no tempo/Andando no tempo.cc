68ec5449ff19e722b653be19f8ffff69360822a3
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-11 14:11:54 +0900

    [Bronze IV] Title: Andando no tempo, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13580. Andando no tempo/Andando no tempo.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n[3]; cin >> n[0] >> n[1] >> n[2];
+	sort(n, n + 3);
+	if (n[0] == n[1] || n[1] == n[2]) cout << "S";
+	else if (n[0] + n[1] == n[2]) cout << "S";
+	else cout << "N";
+}
