a3abaac601a3d23f9268fb8993a1099dcf938bbc
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-12 11:25:50 +0900

    [Bronze IV] Title: Basketball One-on-One, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/18198. Basketball One－on－One/Basketball One－on－One.cc
+++ 백준/Bronze/18198. Basketball One－on－One/Basketball One－on－One.cc
@@ -1,15 +1,13 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
 
-string s;
-int a, b;
-
-int main(){
-  cin >> s;
-  for(int i = 0; i < s.size(); i+=2){
-    if(s[i] == 'A') a += s[i+1] - '0';
-    else b += s[i+1] - '0';
-  }
-  if(a > b) cout << "A";
-  else cout << "B";
+int main() {
+	string st; cin >> st;
+	int a(0), b(0);
+	for (int i = 0; i < st.size(); i += 2) {
+		if (st[i] == 'A') a += st[i + 1] - 48;
+		else b += st[i + 1] - 48;
+	}
+	a > b ? cout << "A" : cout << "B";
 }
