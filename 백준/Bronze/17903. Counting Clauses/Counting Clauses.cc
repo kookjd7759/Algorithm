ccfaf78b3e0d0d021bb0d62e527e0ed9e63734fb
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-12 11:17:09 +0900

    [Bronze IV] Title: Counting Clauses, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17903. Counting Clauses/Counting Clauses.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m, query[21][21];
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> query[i][j];
+    if (n >= 8) cout << "satisfactory";
+    else cout << "unsatisfactory";
+}
