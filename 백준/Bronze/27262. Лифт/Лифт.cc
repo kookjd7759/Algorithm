c9a6342db728826a901fb158c28875301751d3af
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-13 15:16:44 +0900

    [Bronze IV] Title: Лифт, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27262. Лифт/Лифт.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int n, k, a, b; cin >> n >> k >> a >> b;
+	cout << ((k - 1) + (n - 1)) * b << " " << (n - 1) * a;
+}
