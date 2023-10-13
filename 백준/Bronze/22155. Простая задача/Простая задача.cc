22cfe081b29f2bb33738c703dd88bf24560e0870
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-13 13:31:49 +0900

    [Bronze IV] Title: Простая задача, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/22155. Простая задача/Простая задача.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n; cin >> n;
+	while (n--) {
+		int a, b; cin >> a >> b;
+		if ((a <= 1 && b <= 2) || (a <= 2 && b <= 1))
+			cout << "Yes\n";
+		else cout << "No\n";
+	}
+}
