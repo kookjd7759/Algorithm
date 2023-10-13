a0baf995760e54be183119552138448ad42b6a6d
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-13 14:17:11 +0900

    [Bronze IV] Title: Skarpetki, Time: 12 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Bronze/26742. Skarpetki/Skarpetki.cc
+++ 백준/Bronze/26742. Skarpetki/Skarpetki.cc
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
 	string st; cin >> st;
 	int a(0), b(0);
 	for (int i = 0; i < st.size(); i++) st[i] == 'B' ? a++ : b++;
