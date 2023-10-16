6bfde6d3968afb7a0d3711ad03fcd8e3b078fe4e
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-16 09:50:40 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	int n; cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n - i; j++) cout << " ";
+
+		if (i == 1) cout << "*";
+		else if (i == n) for (int j = 0; j < (n * 2 - 1); j++) cout << "*";
+		else {
+			cout << "*";
+			for (int j = 0; j < (i * 2 - 3); j++) cout << " ";
+			cout << "*";
+		}
+		cout << "\n";
+	}
+}
