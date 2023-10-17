d909a9062fb36eb16458f107905761cbf064a0ca
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-17 16:13:30 +0900

    [Bronze III] Title: 평균 중앙값 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5691. 평균 중앙값 문제/평균 중앙값 문제.cc
+++ 백준/Bronze/5691. 평균 중앙값 문제/평균 중앙값 문제.cc
@@ -7,7 +7,7 @@ int main() {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	while (true) {
 		int a, b; cin >> a >> b;
-		if (a == 0 && b == 0) break;
+		if (a + b == 0) break;
 
 		cout << a - (b - a) << "\n";
 	}
