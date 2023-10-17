af07857bbcf842555b0f553989cb4711526dc7d2
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-17 16:39:04 +0900

    [Bronze III] Title: 직각 삼각형의 두 변, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/6322. 직각 삼각형의 두 변/직각 삼각형의 두 변.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cout << fixed; cout.precision(3);
+	int idx(0);
+	while (true) {
+		int a, b, c; cin >> a >> b >> c;
+		if (a == 0 && b == 0 && c == 0) break;
+		idx++;
+
+		char ver;
+		double ret;
+		if (a == -1) {
+			ver = 'a';
+			ret = (double)(c * c - b * b);
+		}
+		else if (b == -1) {
+			ver = 'b';
+			ret = (double)(c * c - a * a);
+		}
+		else {
+			ver = 'c';
+			ret = (double)(a * a + b * b);
+		}
+		cout << "Triangle #" << idx << "\n";
+		if (ret <= 0.0) cout << "Impossible.";
+		else cout << ver << " = " << sqrt(ret);
+		cout << "\n\n";
+	}
+}
