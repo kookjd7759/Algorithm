93881d29c50808b597f7fb0c429cb29e224c2b53
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-17 14:52:42 +0900

    [Bronze III] Title: 캥거루 세마리2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11034. 캥거루 세마리2/캥거루 세마리2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int a, b, c;
+	while (cin >> a >> b >> c) {
+		if (c - b == 1 && b - a == 1) cout << 0;
+		else cout << max(c - b, b - a) - 1;
+		cout << "\n";
+	}
+}
