ebf5167312b16e6dbc35522f3a8b735ccae763ce
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-18 14:46:26 +0900

    [Bronze III] Title: 겨울왕국 티켓 예매, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18247. 겨울왕국 티켓 예매/겨울왕국 티켓 예매.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t;  cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n, m; cin >> n >> m;
+        if (n < 12 || m < 4) cout << -1;
+        else  cout << m * 11 + 4;
+        cout << "\n";
+    }
+}
