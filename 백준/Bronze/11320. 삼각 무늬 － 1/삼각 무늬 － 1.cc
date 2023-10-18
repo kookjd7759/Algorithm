6cdcbd2ce1d40c874f893bfe2e3c7fba60939160
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-18 14:31:40 +0900

    [Bronze III] Title: 삼각 무늬 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11320. 삼각 무늬 － 1/삼각 무늬 － 1.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t;  cin >> t;
+    while (t--) {
+        int a, b; cin >> a >> b;
+        cout << (int)ceil((a * a) / (b * b)) << '\n';
+    }
+}
