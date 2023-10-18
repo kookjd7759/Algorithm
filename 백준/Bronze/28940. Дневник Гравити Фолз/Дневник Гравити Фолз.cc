5e73b370491285a0e2a77c045622e25ad111cdc0
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-18 09:26:56 +0900

    [Bronze IV] Title: Дневник Гравити Фолз, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/28940. Дневник Гравити Фолз/Дневник Гравити Фолз.cc
+++ 백준/Bronze/28940. Дневник Гравити Фолз/Дневник Гравити Фолз.cc
@@ -7,7 +7,7 @@ int main() {
 	int w, h; cin >> w >> h; 
 	int n, a, b; cin >> n >> a >> b;
 	int onepage = (w / a) * (h / b);
-	if (w < a || h < b || onepage == 0) cout << -1;
+	if (onepage == 0) cout << -1;
 	else {
 		n % onepage == 0 ? cout << n / onepage : cout << n / onepage + 1;
 	}
