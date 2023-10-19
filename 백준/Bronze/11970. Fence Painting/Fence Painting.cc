c1a7615b1ffbc587d4214739097c5a982e3897e2
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-19 13:39:20 +0900

    [Bronze III] Title: Fence Painting, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11970. Fence Painting/Fence Painting.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int ret(0), fence[101], a[4]; memset(fence, 0, sizeof(fence));
+	cin >> a[0] >> a[1] >> a[2] >> a[3];
+	for (int i = a[0]; i < a[1]; i++) fence[i]++;
+	for (int i = a[2]; i < a[3]; i++) fence[i]++;
+	for (int i = 0; i <= 100; i++) if (fence[i]) ret++;
+	cout << ret;;
+}
