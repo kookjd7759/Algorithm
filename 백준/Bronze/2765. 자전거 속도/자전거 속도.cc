75fcf743cd08b45f34e36f2292540c18975ec559
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-19 13:58:59 +0900

    [Bronze III] Title: 자전거 속도, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Bronze/2765. 자전거 속도/자전거 속도.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int f[100001];
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cout << fixed; cout.precision(2);
+	int cnt(0);
+	while (true) {
+		cnt++;
+		double d, r, t; cin >> d >> r >> t;
+		if (!r) break;
+		double m = d / (5280.0 * 12.0) * r * 3.14159265358979;
+		cout << "Trip #" << cnt << ": ";
+		cout << m << " " << m / t * 3600 << "\n";
+	}
+}
