d66f2dae6a869548eb56a5c41558353b639f1897
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-24 09:13:21 +0900

    [Bronze III] Title: 홀짝 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25629. 홀짝 수열/홀짝 수열.cc
+++ 백준/Bronze/25629. 홀짝 수열/홀짝 수열.cc
@@ -8,13 +8,10 @@ int main() {
 	sync;
 
 	int n; cin >> n;
-	int oddSize = (n & 1) ? n / 2 + 1 : n / 2;
-	int od(0), ev(0);
+	int od(0);
 	for (int i = 0; i < n; i++) {
 		int a; cin >> a;
 		if (a & 1) od++;
-		else ev++;
 	}
-	if (od == oddSize) cout << 1;
-	else cout << 0;
+	od == ((n & 1) ? n / 2 + 1 : n / 2) ? cout << 1 : cout << 0;
 }
