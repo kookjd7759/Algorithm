035d0e7e9461328b3285eab35d1ff2482e45da8f
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-24 10:39:48 +0900

    [Bronze II] Title: 완전제곱수, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Bronze/1977. 완전제곱수/완전제곱수.cc
+++ 백준/Bronze/1977. 완전제곱수/완전제곱수.cc
@@ -5,12 +5,12 @@
 
 using namespace std;
 
-int arr[10001];
+bool arr[10001];
 
 int main() {
 	sync;
-	memset(arr, 0, sizeof(arr));
-	for (int i = 1; i * i <= 10000; i++) arr[i * i] = i;
+	memset(arr, false, sizeof(arr));
+	for (int i = 1; i * i <= 10000; i++) arr[i * i] = true;
 
 	int m, n; cin >> m >> n;
 	int sum(0), min_(2e9);
