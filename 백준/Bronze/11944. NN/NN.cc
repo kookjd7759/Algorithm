ea4c3a6614a33d61313c984c9b5b8b39bb063cd4
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-27 09:59:33 +0900

    [Bronze II] Title: NN, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11944. NN/NN.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <cmath>
+
+#include <vector>
+#include <stack>
+#include <queue>
+
+#define Sync ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	string st; int n, m, idx(0), ncnt(0), mcnt(0); in st >> m;
+	n = stoi(st);
+	while (ncnt < n && mcnt < m) {
+		out st[idx];
+		idx++; mcnt++;
+		if (idx == st.size()) {
+			idx = 0;
+			ncnt++;
+		}
+	}
+}
