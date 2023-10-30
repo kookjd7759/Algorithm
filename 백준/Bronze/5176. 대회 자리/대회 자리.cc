9335e631e68b1b2b3f755d2fa67cfbfba41c7214
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-30 14:13:12 +0900

    [Bronze II] Title: 대회 자리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5176. 대회 자리/대회 자리.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <cmath>
+
+#include <vector>
+#include <stack>
+#include <queue>
+
+#define Sync ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+#define Roop(x) while(x--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	bool set[501];
+	Testcase{ memset(set, false, sizeof(set));
+	int p, m, ret(0); in p >> m;
+	Roop(p) {
+		int a; in a; 
+		if (set[a]) ret++;
+		else set[a] = true;
+	}
+	out ret endl;
+	}
+}
