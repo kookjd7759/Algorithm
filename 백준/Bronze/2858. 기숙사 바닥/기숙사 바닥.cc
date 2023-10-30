f662a76aee8757367a31c6ba17a41e68d79de36b
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-30 13:16:57 +0900

    [Bronze II] Title: 기숙사 바닥, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2858. 기숙사 바닥/기숙사 바닥.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <cmath>
+
+#include <vector>
+#include <stack>
+#include <queue>
+
+#define Sync ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+    int a, b, i(2); in a >> b;
+    while (i++){
+        for (int j = i; j >= 3; j--) {
+            int x, y;
+            y = (i - 2) * (j - 2);
+            x = i * j - y;
+
+            if (x == a && y == b) {
+                cout << i << ' ' << j;
+                return 0;
+            }
+        }
+    }
+}
