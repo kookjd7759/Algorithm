51ba6df167ca64a34bf992198e29281f403a18f0
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-31 16:01:16 +0900

    [Bronze III] Title: Since 1973, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/28135. Since 1973/Since 1973.cc
+++ 백준/Bronze/28135. Since 1973/Since 1973.cc
@@ -3,12 +3,12 @@
 int main() {
 	int n, ret(0);
 	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		if (i % 100 == 50) ret += 1;
-		else if (i % 1000 / 10 == 50) ret += 1; 
-		else if (i % 10000 / 100 == 50) ret += 1; 
-		else if (i % 100000 / 1000 == 50) ret += 1; 
-		else if (i % 1000000 / 10000 == 50) ret += 1; 
+	for (int i = 50; i < n; i++) {
+		if (i % 100 == 50) ret++;
+		else if (i % 1000 / 10 == 50) ret++; 
+		else if (i % 10000 / 100 == 50) ret++; 
+		else if (i % 100000 / 1000 == 50) ret++; 
+		else if (i % 1000000 / 10000 == 50) ret++;
 	}
 	printf("%d", n + ret);
 }
