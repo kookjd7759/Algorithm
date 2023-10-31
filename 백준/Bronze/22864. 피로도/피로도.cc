d0e77bc0c6e6283909bd9320a5724a04de8c6917
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-31 10:05:29 +0900

    [Bronze II] Title: 피로도, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/22864. 피로도/피로도.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <cmath>
+
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+#define Roop(x) while(x--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	int A, B, C, M; in A >> B >> C >> M;
+	int tired = 0, work = 0;
+	Fori(24) {
+		if (tired + A <= M) {
+			tired += A;
+			work += B;
+		}
+		else tired -= C;
+
+		if (tired < 0) tired = 0;
+	}
+	out work;
+}
