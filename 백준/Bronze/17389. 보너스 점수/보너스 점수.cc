a8754106435eab8c3b1a164546bd5e7b7a3af11c
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-31 14:12:31 +0900

    [Bronze II] Title: 보너스 점수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17389. 보너스 점수/보너스 점수.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cctype>
+#include <string>
+#include <cmath>
+
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+#define Roop(x) while(x--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	int n, ret(0), bonus(0); in n;
+	For1i(n) {
+		char c; in c;
+		if (c == 'O') {
+			ret += i + bonus;
+			bonus++;
+		}
+		else bonus = 0;
+	}
+	out ret;
+}
