8c29ed589c09fa771d652ac26ac43da5859ec384
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-10-31 13:06:52 +0900

    [Bronze II] Title: 벨트, Time: 0 ms, Memory: 2020 KB, Score: 13 point -BaekjoonHub

---
+++ 백준/Bronze/10834. 벨트/벨트.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <cmath>
+
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+#define Roop(x) while(x--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	int M; in M;
+	int ret(1), rotate(0);
+	Roop(M) {
+		int a, b, c; in a >> b >> c;
+		ret *= (double)b / (double)a;
+		rotate += c;
+	}
+	out(rotate & 1) spc ret;
+}
