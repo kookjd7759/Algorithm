596fefa52f79a6f0f16f48ae5b20daf60388a849
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-06 15:29:13 +0900

    [Bronze III] Title: Manhattan, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13221. Manhattan/Manhattan.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+#define Roop(x) while(x--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int x, y; in x >> y;
+    int x_, y_, mini(2e9);
+    Testcase{
+    int a, b; in a >> b;
+    int dis = abs(x - a) + abs(y - b);
+    if (mini > dis) {
+        mini = dis;
+        x_ = a;
+        y_ = b;
+    }
+    };
+    out x_ spc y_;
+}
