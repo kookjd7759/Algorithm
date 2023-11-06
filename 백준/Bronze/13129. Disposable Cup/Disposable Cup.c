fcdabc4c2ed8110566477ca88d1c0d3a34cd6493
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-06 15:06:29 +0900

    [Bronze III] Title: Disposable Cup, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/13129. Disposable Cup/Disposable Cup.c
+++ 백준/Bronze/13129. Disposable Cup/Disposable Cup.c
@@ -1,4 +1,1 @@
-main() {
-	int a, b, n; scanf("%d %d %d", &a, &b, &n);
-	for (int i = 0; i < n; i++) printf("%d ", a * n + (i + 1) * b);
-}
+int a,b,n,i=1;main(){scanf("%d%d%d",&a,&b,&n);for (i;i<=n;i++)printf("%d ",a*n+i*b);}
