ccd72eebbd5df30202d74fbc84d9f7bb65ea127f
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-06 15:41:01 +0900

    [Bronze III] Title: The REAL Manhattan distance, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13228. The REAL Manhattan distance/The REAL Manhattan distance.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+#define Roop(x) while(x--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    Testcase{
+    int b1[3], b2[3];
+    Fori(3) in b1[i];
+    Fori(3) in b2[i];
+    out b1[2] + b2[2] + abs(b1[0] - b2[0]) + abs(b1[1] - b2[1]) endl;
+    };
+}
