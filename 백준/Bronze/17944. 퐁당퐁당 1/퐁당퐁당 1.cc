b30130f30598b2b4c501e9a3912e028b22e43ec8
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-06 14:46:12 +0900

    [Bronze III] Title: 퐁당퐁당 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/17944. 퐁당퐁당 1/퐁당퐁당 1.cc
+++ 백준/Bronze/17944. 퐁당퐁당 1/퐁당퐁당 1.cc
@@ -1,14 +1,8 @@
-#include <bits/stdc++.h>
-
-using namespace std;
+#include <stdio.h>
 
 int main() {
     int n, t; scanf("%d %d", &n, &t);
-    while (n * 4 - 2 < t) t -= n * 4 - 2;
-
-    if (n * 2 >= t) printf("%d", t);
-    else printf("%d", n * 4 - t);
+    int n4 = n * 4;
+    while (n4 - 2 < t) t -= n4 - 2;
+    printf("%d", (n * 2 >= t ? t : n4 - t));
 }
-/*
-n * 2 - (t - (n * 2)) = n * 4 - t;
-*/
