d5453107fe60dde8b11156ae55b9a6e16882a328
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-06 14:43:02 +0900

    [Bronze III] Title: 퐁당퐁당 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17944. 퐁당퐁당 1/퐁당퐁당 1.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+#define Roop(x) while(x--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int n, t; in n >> t;
+    while (n * 4 - 2 < t) {
+        t -= n * 4 - 2;
+    }
+
+    if (n * 2 >= t) out t;
+    else out n * 2 - (t - (n * 2));
+}
+/*
+* 2
+123 4 32 123 4
+* 3
+12345 6 5432 12345 6
+*/
