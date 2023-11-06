98148fe81a0e138933d7ff771ee3b21a4b85f673
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-06 13:57:27 +0900

    [Bronze III] Title: 그래서 님 푼 문제 수가?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29720. 그래서 님 푼 문제 수가？/그래서 님 푼 문제 수가？.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+#define Roop(x) while(x--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+    
+    int n, m, k; in n >> m >> k;
+    if (n - (m * k) <= 0) out 0 spc n - (m * k) + (m - 1);
+    else out n - (m * k) spc n - (m * k) + (m - 1);
+}
