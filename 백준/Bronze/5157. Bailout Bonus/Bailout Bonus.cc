51428b46f5c1adc2093d81f639e1861839b8b323
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-07 17:18:04 +0900

    [Bronze III] Title: Bailout Bonus, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Bronze/5157. Bailout Bonus/Bailout Bonus.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+#define Roop(x) while(x--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int t; in t;
+    For1i(t) {
+        int a, b, c, d; in a >> b >> c >> d;
+
+        set<int> s;
+        Forj(b) {
+            int c; in c; s.insert(c);
+        }
+
+        ll ret = 0;
+        Forj(c) {
+            int idx, bo; cin >> idx >> bo;
+
+            if (s.find(idx) != s.end()) ret += (ll)(bo) * d / 100;
+        }
+
+        out "Data Set " << i << ":\n";
+        out ret endl;
+        if (i != b) out "\n";
+    }
+}
