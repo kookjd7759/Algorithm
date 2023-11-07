22ef90112a5af13978cd42edc1f4c4594057aabf
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-07 10:19:22 +0900

    [Bronze III] Title: Golf, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6437. Golf/Golf.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+#define Roop(x) while(x--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int idx = 1;
+    while (true) {
+        int a, b; in a >> b;
+        if (!a) break;
+
+        out "Hole #" << idx++ endl;
+
+        if (b == 1) out "Hole-in-one.";
+        else if (b <= a - 3) out "Double eagle.";
+        else if (b == a - 2) out "Eagle.";
+        else if (b == a - 1) out "Birdie.";
+        else if (b == a) out "Par.";
+        else if (b == a + 1) out "Bogey.";
+        else out "Double Bogey.";
+        ent; ent;
+    }
+}
