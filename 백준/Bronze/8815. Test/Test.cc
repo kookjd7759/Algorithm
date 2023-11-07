87335b901c1125d714df5d0d54501b28253e926e
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-07 14:34:35 +0900

    [Bronze III] Title: Test, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/8815. Test/Test.cc
+++ 백준/Bronze/8815. Test/Test.cc
@@ -1,31 +1,10 @@
-#include <bits/stdc++.h>
-
-#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
-#define Fixed(x) cout << fixed; cout.precision(x);
-#define ll long long
-#define in cin >>
-#define out cout <<
-#define spc << " " <<
-#define endl << "\n"
-#define ent cout << "\n"
-#define Fori(x) for (int i = 0; i < x; i++)
-#define Forj(x) for (int j = 0; j < x; j++)
-#define Fork(x) for (int k = 0; k < x; k++)
-#define For1i(x) for (int i = 1; i <= x; i++)
-#define For1j(x) for (int j = 1; j <= x; j++)
-#define For1k(x) for (int k = 1; k <= x; k++)
-#define Testcase int tc; in tc; while(tc--) 
-#define Roop(x) while(x--) 
-
-using namespace std;
+#include <stdio.h>
 
 int main() {
-    Sync;
-
-    Testcase{
-    string st = "BABCBCDCDADAB";
-    int n; in n; n %= 12;
-    out st[n] endl;
-    };
-    
+    int t; scanf("%d", &t);
+    while (t--) {
+        int n; scanf("%d", &n);
+        n %= 12;
+        printf("%c\n", "BABCBCDCDADAB"[n]);
+    }
 }
