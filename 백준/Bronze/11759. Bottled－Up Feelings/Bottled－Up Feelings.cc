a5cd247691f082d35160d82a97548a2ab0432cea
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-07 14:00:00 +0900

    [Bronze III] Title: Bottled-Up Feelings, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11759. Bottled－Up Feelings/Bottled－Up Feelings.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+#define Roop(x) while(x--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int n, a, b; in n >> a >> b;
+    int am = n / a;
+    bool ch = false;
+    for (int i = am; i >= 0; i--) {
+        if ((n - a * i) % b == 0) {
+            out i spc(n - a * i) / b;
+            ch = true;
+            break;
+        }
+    }
+    if (!ch) out "Impossible";
+}
