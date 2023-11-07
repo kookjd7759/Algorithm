a14e9aafe7bcf660e3310d2f8ff87ccc507afd6d
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-07 14:11:28 +0900

    [Bronze III] Title: 99 Problems, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25270. 99 Problems/99 Problems.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+#define Roop(x) while(x--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int n; in n;
+    int up = n, down = n;
+    while (true) {
+        if (up % 10 == 9 && up % 100 / 10 == 9) {
+            out up;
+            break;
+        }
+        if (down % 10 == 9 && down % 100 / 10 == 9) {
+            out down;
+            break;
+        }
+        up++;
+        down--;
+    }
+}
