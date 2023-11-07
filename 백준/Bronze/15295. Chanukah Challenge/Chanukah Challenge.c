87301564a7393e728adcd15b8de4bdc445e750b0
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-07 13:36:29 +0900

    [Bronze III] Title: Chanukah Challenge, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/15295. Chanukah Challenge/Chanukah Challenge.c
+++ 백준/Bronze/15295. Chanukah Challenge/Chanukah Challenge.c
@@ -1,5 +1,5 @@
-main() {
-    int t; scanf("%d", &t);
+main(t) {
+    scanf("%d", &t);
     while (t--) {
         int k, n; scanf("%d%d", &k, &n);
         printf("%d %d\n", k, (1 + n) * n / 2 + n);
