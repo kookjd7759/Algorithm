0a2d609064923b2fb64ae9fb2b30254d42fc5d1e
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-07 13:48:46 +0900

    [Bronze III] Title: Jumbled Compass, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13363. Jumbled Compass/Jumbled Compass.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+#define Roop(x) while(x--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int a, b; in a >> b;
+    int clo, nonclo;
+    if (a <= b) clo = b - a;
+    else clo = 360 - a + b;
+
+    if (a < b) nonclo = a + 360 - b;
+    else nonclo = a - b;
+    nonclo = -nonclo;
+
+    if (clo <= abs(nonclo)) out clo;
+    else out nonclo;
+}
