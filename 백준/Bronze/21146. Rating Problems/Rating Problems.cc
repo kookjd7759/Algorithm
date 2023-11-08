79ebb42b15c6fd545313a458ab951f851475e852
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-08 13:58:19 +0900

    [Bronze III] Title: Rating Problems, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21146. Rating Problems/Rating Problems.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+#define Roop(x) while(x--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+    Fixed(5);
+
+    int n, k; in n >> k;
+    double mini(0), maxi(0);
+    Fori(k) {
+        int a; in a;
+        maxi += a;
+    }
+    mini = maxi;
+    mini += -3 * (n - k);
+    maxi += 3 * (n - k);
+    out mini / n spc maxi / n;
+}
