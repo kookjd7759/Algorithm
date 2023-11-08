a8db725bee17493936c917d8ec5dfd2c653901ad
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-08 09:50:55 +0900

    [Bronze III] Title: Lobby, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13063. Lobby/Lobby.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+#define Roop(x) while(x--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+	while (true) {
+		int n, m, k; in n >> m >> k;
+		if (!n && !m && !k) break;
+		int ret = n / 2 + 1;
+		if (ret <= m) {
+			out 0 endl;
+			continue;
+		}
+		if (ret - m > n - m - k) out -1 endl;
+		else out ret - m endl;
+	}
+}
