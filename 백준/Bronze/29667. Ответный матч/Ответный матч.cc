34afe587d7da7ab7fa64f45f00cc1be4c137136c
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-09 16:34:37 +0900

    [Bronze III] Title: Ответный матч, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/29667. Ответный матч/Ответный матч.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+#define Roop(x) while(x--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	int a1(0), b1(0), a2(0), b2(0);
+	string game; in game;
+	a1 += (game[0] - '0');
+	b1 += (game[2] - '0');
+
+	in game;
+	a2 += (game[2] - '0');
+	b2 += (game[0] - '0');
+
+	if (a2 <= b1 && b2 <= a1) out"YES";
+	else out "NO";
+}
