83d7cd4776e8cb11ffafdd2b9914ed3f4279f13e
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-10 14:37:21 +0900

    [Bronze III] Title: Just a Joystick, Time: 0 ms, Memory: 2392 KB -BaekjoonHub

---
+++ 백준/Bronze/30314. Just a Joystick/Just a Joystick.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+#define Roop(x) while(x--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	int n, sum(0); in n;
+	string a, b; in a >> b;
+	Fori(n) {
+		sum += min(abs(a[i] - b[i]), 
+			(a[i] > b[i] ? 
+				26 - (a[i] - '0') + (b[i] - '0') : 26 - (b[i] - '0') + (a[i] - '0')));
+	}
+	out sum;
+}
