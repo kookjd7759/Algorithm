62720bf297d3b98ce8086ad31cfc7a9181625db9
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-12 15:34:54 +0900

    [Bronze III] Title: Desni klik, Time: 0 ms, Memory: 2024 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/26392. Desni klik/Desni klik.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed;cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+    
+    int t, a, b; in t >> a >> b;
+    while (t--) {
+        int maxi(-1), mini;
+        for (int i = a; i > 0; i--) {
+            string st; in st;
+            if (st.find('#') != string::npos){
+                if (maxi == -1) maxi = i;
+                mini = i;
+            }
+        }
+        out maxi - mini endl;
+    }
+}
