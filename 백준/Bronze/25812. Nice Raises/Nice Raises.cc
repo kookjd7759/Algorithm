d05d1a4e5677690b86aca97515c2b2f5a382683b
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-13 15:04:17 +0900

    [Bronze III] Title: Nice Raises, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25812. Nice Raises/Nice Raises.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	int n, up; in n >> up;
+	int ret1(0), ret2(0);
+	Fori(n) {
+		int a; in a;
+		if (a * 2 == a + up) continue;
+
+		if (a + up > a * 2) ret1++;
+		else ret2++;
+	}
+
+	if (ret1 == ret2) out 0;
+	else out(ret1 > ret2 ? 1 : 2);
+}
