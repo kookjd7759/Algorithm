03b5a76368dc453b88bf8ecb9fadd3b3dc5b7f0a
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-13 17:45:25 +0900

    [Bronze III] Title: ИЗЛОЖЕНИЕ НА ПЧЕЛЕН МЕД, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24387. ИЗЛОЖЕНИЕ НА ПЧЕЛЕН МЕД/ИЗЛОЖЕНИЕ НА ПЧЕЛЕН МЕД.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+    vector<ll> a(3), b(3);
+    Fori(3) in a[i]; sort(a.begin(), a.end());
+    Fori(3) in b[i]; sort(b.begin(), b.end());
+    ll ret(0);
+    Fori(3) ret += (a[i] * b[i]);
+    out ret;
+}
