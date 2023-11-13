566ffab7390b349b30081887ffbacfd27feabfb3
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-13 13:28:55 +0900

    [Bronze III] Title: Special Event, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28288. Special Event/Special Event.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	int sum[6]; memset(sum, 0, sizeof(sum));
+	int n; in n;
+	Fori(n) {
+		For1j(5) {
+			char c; in c;
+			if (c == 'Y') sum[j]++;
+		}
+	}
+	int maxi(-1);
+	For1i(5) maxi = max(maxi, sum[i]);
+
+	bool ch = false;
+	For1i(5) {
+		if (maxi == sum[i]) {
+			if (ch) out ",";
+			out i;
+			ch = true;
+		}
+	}
+}
