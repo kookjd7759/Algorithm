541d87032c672c292f5430cca928eafe9698c85d
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-13 11:36:15 +0900

    [Bronze III] Title: Extreme Temperatures, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/18309. Extreme Temperatures/Extreme Temperatures.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+	
+	string st; 
+	int mini(2e9), maxi(-2e9);
+	while (in st) {
+		if (st.size() == 10) continue;
+		mini = min(mini, stoi(st));
+		maxi = max(maxi, stoi(st));
+	}
+	out mini spc maxi;
+}
