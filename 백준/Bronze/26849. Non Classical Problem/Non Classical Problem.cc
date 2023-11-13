58ebf0c38232780c6e4469ea725c9896d2762f57
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-13 15:47:23 +0900

    [Bronze III] Title: Non Classical Problem, Time: 52 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26849. Non Classical Problem/Non Classical Problem.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+	Fixed(10);
+
+	double mini(2e9), maxi(-2e9), sum(0);
+	Testcase{
+	double a, b; in a >> b;
+	sum += a / b;
+	mini = min(mini, a / b);
+	maxi = max(maxi, a / b);
+	};
+	out mini spc maxi spc sum;
+}
