7f9ad4a9d3b66c69753f66771870488ea7ddee0c
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-14 10:11:13 +0900

    [Bronze III] Title: Fastestest Function, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/25985. Fastestest Function/Fastestest Function.c
+++ 백준/Bronze/25985. Fastestest Function/Fastestest Function.c
@@ -1,6 +1,1 @@
-#include <stdio.h>
-
-int main() {
-	double a, b;
-	scanf("%lf%lf", &a, &b); printf("%.10lf", a / (b * (100 - a) / (100 - b)));
-}
+main(){float a,b;scanf("%f%f",&a,&b);printf("%.6f",a/(b*(100-a)/(100-b)));}
