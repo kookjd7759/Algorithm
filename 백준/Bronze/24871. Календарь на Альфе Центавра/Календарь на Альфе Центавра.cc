1f874e2be1b778a4fb7d2e5eff85c0f5e72c979c
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-14 09:18:48 +0900

    [Bronze III] Title: Календарь на Альфе Центавра, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/24871. Календарь на Альфе Центавра/Календарь на Альфе Центавра.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	ll d, m, w; in d >> m >> w;
+	ll date[3]; Fori(3) in date[i];
+	ll total_d = (date[2] - 1) * m * d + 
+		(date[1] - 1) * d + 
+		date[0];
+	int idx = total_d % w;
+	if (idx == 0) idx = w;
+	out (char)(--idx + 'a');
+}
