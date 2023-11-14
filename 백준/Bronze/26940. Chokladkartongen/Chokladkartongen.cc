60f57934be31886bf0d5ae335d2aa1cdd7360922
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-14 16:09:08 +0900

    [Bronze III] Title: Chokladkartongen, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26940. Chokladkartongen/Chokladkartongen.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+	
+	int n, prev, ret(0); in n >> prev; n--;
+	while (n--) {
+		int a; cin >> a;
+		if (a > prev) ret++;
+		prev = a;
+	}
+	out ret;
+}
