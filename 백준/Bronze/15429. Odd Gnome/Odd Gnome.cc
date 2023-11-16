f0e6f82adff90b2731224ba5e3d74d726d25d84b
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-16 23:20:02 +0900

    [Bronze III] Title: Odd Gnome, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15429. Odd Gnome/Odd Gnome.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed;cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+    
+    Testcase{
+    int k; in k; k--;
+    int prev; in prev;
+    bool ch = false;
+    For1i(k) {
+        int a; in a;
+        if (!ch && a - prev != 1) {
+            out i + 1 endl;
+            ch = true;
+        }
+        prev = a;
+    }
+    };
+}
