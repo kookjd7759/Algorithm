4f31dc58dfb207bef19bd708f329a8d3bd82dc3c
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-16 16:54:07 +0900

    [Bronze III] Title: Changing Strings, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25813. Changing Strings/Changing Strings.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	string st; in st;
+	int l(0), r = st.size() - 1;
+	for (; st[l] != 'U'; l++) st[l] = '-';
+	for (; st[r] != 'F'; r--) st[r] = '-';
+	for (int i = l + 1; i < r; i++) st[i] = 'C';
+	out st;
+}
