da8417500730ab3ffe8d02f67d7d642348cc383b
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-16 16:58:07 +0900

    [Bronze III] Title: Elder, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17042. Elder/Elder.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	bool ch[26]; memset(ch, false, sizeof(ch));
+	int n, cnt(0);
+	char cur;
+	in cur >> n;
+
+	ch[cur - 'A'] = true;
+	while (n--) {
+		char x, y; in x >> y;
+		if (y == cur) ch[x - 'A'] = true, cur = x;
+	}
+
+	Fori(26) if (ch[i]) cnt++;
+	out cur << '\n' << cnt;
+}
