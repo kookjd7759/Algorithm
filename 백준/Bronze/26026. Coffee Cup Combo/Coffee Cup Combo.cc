e419516b44193f272a8ac904bfdfe3cdbdeab046
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-16 15:44:13 +0900

    [Bronze III] Title: Coffee Cup Combo, Time: 0 ms, Memory: 2392 KB -BaekjoonHub

---
+++ 백준/Bronze/26026. Coffee Cup Combo/Coffee Cup Combo.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	int n; string st; in n >> st;
+	int cof(0), ret(0);
+	Fori(n) {
+		if (st[i] == '0') {
+			if (cof) cof--, ret++;
+		}
+		else {
+			cof = 2;
+			ret++;
+		}
+	}
+	out ret;
+}
