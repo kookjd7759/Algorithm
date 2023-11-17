b2b532ede76364095fede6b3b751289f30359b3a
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-17 18:10:43 +0900

    [Bronze II] Title: 팰린드롬, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10174. 팰린드롬/팰린드롬.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed;cout.precision(x);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+    
+    int t; in t;
+    string st; getline(cin, st);
+    while (t--) {
+        getline(cin, st);
+        Fori(st.size()) if (st[i] >= 'A' && st[i] <= 'Z')
+                st[i] += 32;
+
+        bool ch = true;
+        Fori(st.size() / 2) if (st[i] != st[st.size() - i - 1]) {
+            ch = false; break;
+        }
+        out(ch ? "Yes" : "No") endl;
+    };
+}
