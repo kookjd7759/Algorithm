ef9589d93983b67fa5e1c736355a13180f635ca7
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-20 17:15:18 +0900

    [Bronze II] Title: 대충 더해, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Bronze/8949. 대충 더해/대충 더해.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define Interactive cin.tie(0)->sync_with_stdio(0);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	deque<string> deq;
+	string a, b; in a >> b;
+	while (a.size() < b.size()) a = "0" + a;
+	while (a.size() > b.size()) b = "0" + b;
+	for (int i = a.size() - 1; i >= 0; i--)
+		deq.push_front(to_string((a[i] - '0') + (b[i] - '0')));
+	for (const auto& iter : deq)
+		out iter;
+}
