b10fe6cfab63a91300cc4be32fca951789dc6c28
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-21 16:49:55 +0900

    [Bronze II] Title: The Cow-Signal, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14175. The Cow－Signal/The Cow－Signal.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define Interactive cin.tie(0)->sync_with_stdio(0);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+    int m, n, k; in m >> n >> k;
+    char s[10][11];
+    Fori(m) in s[i];
+    Fori(m * k) {
+        Forj(n * k) out s[i / k][j / k]; ent;
+    }
+}
