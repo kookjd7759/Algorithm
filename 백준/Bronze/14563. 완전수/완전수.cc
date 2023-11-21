791a4d013f1b77a9bf816f5a6cb5e739cb44a655
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-21 10:09:12 +0900

    [Bronze II] Title: 완전수, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14563. 완전수/완전수.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define Interactive cin.tie(0)->sync_with_stdio(0);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+    int t; in t;
+    Fori(t) {
+        int n, sum(0); in n;
+        For1j(n - 1) if (n % j == 0) 
+                sum += j;
+
+        if (sum == n) out "Perfect";
+        else if (sum > n) out "Abundant";
+        else out "Deficient";
+        ent;
+    }
+}
