c4fd76465c6caaf0404e5f27c94d287722f13cef
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-21 10:11:53 +0900

    [Bronze II] Title: ZOAC 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/18238. ZOAC 2/ZOAC 2.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define Interactive cin.tie(0)->sync_with_stdio(0);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	string st; in st;
+	char cur = 'A';
+	int a(0), b(0), ret(0);
+	Fori(st.size()) {
+		a = abs(st[i] - cur);
+		b = 26 - a;
+		ret += min(a, b);
+		cur = st[i];
+	}
+	out ret endl;
+}
