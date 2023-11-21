ee7240202052e4efea39690959208ad9c5ff9981
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-21 11:23:34 +0900

    [Bronze II] Title: 복호화, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9046. 복호화/복호화.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define Interactive cin.tie(0)->sync_with_stdio(0);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	int t; in t;
+	string st; getline(cin, st);
+	while (t--) {
+		getline(cin, st);
+		int eng[26]; memset(eng, 0, sizeof(eng));
+		int maxi(0), cnt(0);
+		Fori(st.size()) 
+			if ('a' <= st[i] && st[i] <= 'z') {
+				eng[st[i] - 'a']++;
+				maxi = max(maxi, eng[st[i] - 'a']);
+			}
+		Fori(26) if (maxi == eng[i]) cnt++;
+		if (cnt == 1) {
+			Fori(26) if (maxi == eng[i]) out(char)(i + 'a') endl;
+		}
+		else out "?\n";
+	}
+}
