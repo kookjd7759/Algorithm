db64389f8cffe7062085db87e02515e49535ceea
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-21 16:48:37 +0900

    [Bronze II] Title: 숫자의 개수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21567. 숫자의 개수 2/숫자의 개수 2.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define Interactive cin.tie(0)->sync_with_stdio(0);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	ll a, b, c; in a >> b >> c;
+	int cnt[10]; memset(cnt, 0, sizeof cnt);
+	ll sum = a * b * c;
+	while (sum > 0) cnt[sum % 10]++, sum /= 10;
+	Fori(10) out cnt[i] endl;
+}
