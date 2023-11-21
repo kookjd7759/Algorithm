e1fc906adeb3f9d4b2e93be5a01a61793b9981aa
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-21 16:57:00 +0900

    [Bronze II] Title: 모르고리즘 회장님 추천 받습니다, Time: 40 ms, Memory: 5936 KB -BaekjoonHub

---
+++ 백준/Bronze/20124. 모르고리즘 회장님 추천 받습니다/모르고리즘 회장님 추천 받습니다.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define Interactive cin.tie(0)->sync_with_stdio(0);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	int n; in n;
+	vector<pair<string, int>> vec(n);
+	Fori(n) in vec[i].first >> vec[i].second;
+	sort(vec.begin(), vec.end(),
+		[&](const pair<string, int>& a, const pair<string, int>& b) {
+			if (a.second == b.second)
+				return a.first < b.first;
+			else
+				return a.second > b.second;
+		});
+	out vec[0].first;
+}
