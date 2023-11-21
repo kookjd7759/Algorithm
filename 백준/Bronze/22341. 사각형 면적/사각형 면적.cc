5e817bfc9dc8484b9cd901e817022ba69506392b
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-21 11:41:27 +0900

    [Bronze II] Title: 사각형 면적, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/22341. 사각형 면적/사각형 면적.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define Fixed(x) cout << fixed; cout.precision(x);
+#define Interactive cin.tie(0)->sync_with_stdio(0);
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+    int n, c, cy, cx; in n >> c;
+    cy = n, cx = n;
+    while (c--) {
+        int y, x; in y >> x;
+        if (y > cy || x > cx) continue;
+        int ny = y, nx = cx, w = y * cx;
+        if (w < cy * x) ny = cy, nx = x;
+        cy = ny, cx = nx;
+    }
+    out cy* cx;
+}
