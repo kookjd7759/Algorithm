b90b68ac4521f79652fe550d6fe2cddea3cb6c31
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-22 13:44:36 +0900

    [Bronze II] Title: 스시, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/20494. 스시/스시.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	int cnt[26]; memset(cnt, 0, sizeof cnt);
+	Testcase{
+	string st; in st;
+	Fori(st.size()) cnt[st[i] - 'A']++;
+	};
+	int sum(0);
+	Fori(26) sum += cnt[i];
+	out sum;
+}
