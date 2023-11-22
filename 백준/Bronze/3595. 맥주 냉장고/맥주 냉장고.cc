1f2b1eabc88c635cbc7efce1e9f8876ec3f4e0ea
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-22 14:46:36 +0900

    [Bronze II] Title: 맥주 냉장고, Time: 312 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3595. 맥주 냉장고/맥주 냉장고.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	int n; in n;
+	int mini(2e9), ret[3] = { 1,1,1 };
+	For1i(n / 2) {
+		if (n % i == 0) {
+			For1j(n / 2) {
+				if (n % (i * j) == 0) {
+					int k = n / (i * j);
+					if (mini > (i * j) * 2 + (i * k) * 2 + (j * k) * 2) {
+						mini = (i * j) * 2 + (i * k) * 2 + (j * k) * 2;
+						ret[0] = i;
+						ret[1] = j;
+						ret[2] = k;
+					}
+				}
+			}
+		}
+	}
+	out ret[0] spc ret[1] spc ret[2];
+}
