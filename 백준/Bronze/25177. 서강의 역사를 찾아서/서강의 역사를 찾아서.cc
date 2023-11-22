b9407a4147fb6f9e59db3bc91fc465497caf5d9f
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-22 13:50:54 +0900

    [Bronze II] Title: 서강의 역사를 찾아서, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25177. 서강의 역사를 찾아서/서강의 역사를 찾아서.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+	int n, m; in n >> m;
+	int arr1[1001], arr2[1001]; 
+	memset(arr1, 0, sizeof arr1); memset(arr2, 0, sizeof arr2);
+	For1i(n) in arr1[i];
+	For1i(m) in arr2[i];
+	int maxi(0);
+	For1i(max(n, m)) maxi = max(maxi, arr2[i] - arr1[i]);
+	out maxi;
+}
