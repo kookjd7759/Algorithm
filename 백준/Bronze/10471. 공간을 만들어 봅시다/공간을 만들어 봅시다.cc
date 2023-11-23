931e8042be5958a7f55336aa2707a50a969f5581
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-23 15:22:28 +0900

    [Bronze II] Title: 공간을 만들어 봅시다, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10471. 공간을 만들어 봅시다/공간을 만들어 봅시다.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	int n, w; in n >> w;
+	vector<int> vec(w + 2); vec[0] = 0, vec[vec.size() - 1] = n;
+	For1i(w) in vec[i];
+
+	set<int> s;
+	Fori(w + 1){
+		for (int j = i + 1; j < vec.size(); j++)
+			s.insert(vec[j] - vec[i]);
+	}
+	for (int iter : s) out iter << " ";
+}
