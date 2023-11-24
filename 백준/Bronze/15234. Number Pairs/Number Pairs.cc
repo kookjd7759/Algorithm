7ee773c39beeb5cdc73e7a0634f38c36af0c9a5a
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-24 16:31:48 +0900

    [Bronze II] Title: Number Pairs, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15234. Number Pairs/Number Pairs.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+    
+    int n, k; in n >> k;
+    vector<int> vec(n); Fori(n) in vec[i];
+    sort(vec.begin(), vec.end());
+
+    int cnt(0);
+    Fori(n - 1) for (int j = i + 1; j < n; j++) {
+        if (vec[i] + vec[j] == k) cnt++;
+    }
+    out cnt;
+}
