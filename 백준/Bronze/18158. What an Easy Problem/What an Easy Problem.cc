e64ac045fbbe9c359346dc51f91a9897c90821d8
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-24 15:39:28 +0900

    [Bronze II] Title: What an Easy Problem, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/18158. What an Easy Problem/What an Easy Problem.cc
+++ 백준/Bronze/18158. What an Easy Problem/What an Easy Problem.cc
@@ -1,11 +1,7 @@
 #include "WAEP.h"
 
-void init(int T) {
-	return;
-}
+void init(int T) {}
 
 int janken(int P) {
-	if (P == 0) return 0;
-	else if (P == 2) return 2;
-	else return 5;
+	return P;
 }
