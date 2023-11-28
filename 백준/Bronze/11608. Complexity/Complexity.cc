7330bc878a238a0e51fc644793bb4700bea066c8
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-28 10:28:31 +0900

    [Bronze II] Title: Complexity, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11608. Complexity/Complexity.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+    
+    string st; in st;
+    int eng[26], cnt(0), ret(0); memset(eng, 0, sizeof eng);
+    vector <int> vec;
+    
+    Fori(st.size()) eng[st[i] - 'a']++;
+    Fori(26) if (eng[i]) vec.push_back(eng[i]), cnt++;
+    sort(vec.begin(), vec.end());
+
+    Fori(cnt - 2) ret += vec[i];
+    out ret;
+}
