c790082edf570a2c989b9f0a4744adc95470920c
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-11-30 13:00:53 +0900

    [Bronze II] Title: New Year and Naming, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/18884. New Year and Naming/New Year and Naming.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int n, m; in n >> m;
+    vector<string> vec1(n), vec2(m);
+    Fori(n) in vec1[i];
+    Fori(m) in vec2[i];
+
+    Testcase{
+    int a; in a;
+    int idx1 = (a - 1) % n, idx2 = (a - 1) % m;
+    out vec1[idx1] << vec2[idx2] endl;
+    };
+}
