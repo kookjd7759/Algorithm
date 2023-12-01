32567ad81a627696f846a583197ce132c59b2bb0
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-12-01 09:33:46 +0900

    [Bronze II] Title: ICPC, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/14004. ICPC/ICPC.c
+++ 백준/Bronze/14004. ICPC/ICPC.c
@@ -1,5 +1,1 @@
-main() {
-    long long a, b, c, d;
-    scanf("%lld%lld%lld%lld", &a, &b, &c, &d);
-    printf("%lld", (c + d) / (a - b));
-}
+main(){long long a,b,c,d;scanf("%lld%lld%lld%lld",&a,&b,&c,&d);printf("%lld",(c+d)/(a-b));}
