bb54a378d6226eca97906a1f84f91ecbb8ad555b
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-12-14 09:31:44 +0900

    [Bronze III] Title: UOSPC 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30822. UOSPC 세기/UOSPC 세기.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int uospc[5]; memset(uospc, 0, sizeof uospc);
+    int n; in n;
+    Fori(n) {
+        char c; in c;
+        if (c == 'u') uospc[0]++;
+        else if (c == 'o') uospc[1]++;
+        else if (c == 's') uospc[2]++;
+        else if (c == 'p') uospc[3]++;
+        else if (c == 'c') uospc[4]++;
+    }
+    int mini(2e9);
+    Fori(5) mini = min(mini, uospc[i]);
+    out mini;
+}
