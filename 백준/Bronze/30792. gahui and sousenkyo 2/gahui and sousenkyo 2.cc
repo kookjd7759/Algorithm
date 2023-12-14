38c20ab3f3888c76c94bc269a14a16c225bffd45
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-12-14 09:28:31 +0900

    [Bronze III] Title: gahui and sousenkyo 2, Time: 4 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Bronze/30792. gahui and sousenkyo 2/gahui and sousenkyo 2.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int n, Gahui; in n >> Gahui;
+    vector<int> vec(n); vec[0] = Gahui;
+    For1i(n - 1) in vec[i];
+    sort(vec.begin(), vec.end(), [&](const int& a, const int& b) {
+        return a > b;
+        });
+
+    Fori(n) if (vec[i] == Gahui) {
+        out i + 1;
+        break;
+    }
+}
