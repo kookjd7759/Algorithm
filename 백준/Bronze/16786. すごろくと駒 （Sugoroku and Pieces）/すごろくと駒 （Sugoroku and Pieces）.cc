3180e97238ed44af462e0d73ccdd2371d58c4da4
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-12-21 16:09:37 +0900

    [Bronze II] Title: すごろくと駒 (Sugoroku and Pieces), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16786. すごろくと駒 （Sugoroku and Pieces）/すごろくと駒 （Sugoroku and Pieces）.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    bool board[2020]; memset(board, 0, sizeof board);
+    int n; in n;
+    vector<int> vec(n);
+    Fori(n) {
+        int a; in a;
+        vec[i] = a;
+        board[a] = true;
+    }
+
+    int m; in m;
+    Fori(m) {
+        int a; in a; a--;
+        int pos = vec[a];
+        if (pos != 2019 && !board[pos + 1]) {
+            board[pos] = false;
+            board[pos + 1] = true;
+            vec[a]++;
+        }
+    }
+
+    Fori(n) out vec[i] endl;
+}
