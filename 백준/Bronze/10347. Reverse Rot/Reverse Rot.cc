ddd4b0a7165e336c5c1997dbb02e7341b4af8735
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-12-22 10:30:53 +0900

    [Bronze II] Title: Reverse Rot, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10347. Reverse Rot/Reverse Rot.cc
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    char rt[28] = {
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F',
+        'G',
+        'H',
+        'I',
+        'J',
+        'K',
+        'L',
+        'M',
+        'N',
+        'O',
+        'P',
+        'Q',
+        'R',
+        'S',
+        'T',
+        'U',
+        'V',
+        'W',
+        'X',
+        'Y',
+        'Z',
+        '_',
+        '.'
+    };
+    while (true) {
+        int n; in n;
+        if (!n) break;
+
+        string st, ans = "";
+        in st;
+        reverse(st.begin(), st.end());
+        Fori(st.size()) {
+            if (isalpha(st[i]))
+                ans += rt[(st[i] - 'A' + n) % 28];
+            else {
+                if (st[i] == '_') ans += rt[(26 + n) % 28];
+                else ans += rt[(27 + n) % 28];
+            }
+        }
+        out ans endl;
+    }
+}
