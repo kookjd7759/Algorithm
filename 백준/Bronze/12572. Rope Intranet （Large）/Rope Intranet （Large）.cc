279a187271703920f301bb5d8108c46cd7d0b7bb
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2023-12-22 10:25:05 +0900

    [Bronze II] Title: Rope Intranet (Large), Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/12572. Rope Intranet （Large）/Rope Intranet （Large）.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int T, N;
+    int X[1000], Y[1000];
+	cin >> T;
+
+	for (int t = 1; t <= T; t++) {
+		cin >> N;
+		for (int i = 0; i < N; i++) cin >> X[i] >> Y[i];
+
+		int ans = 0;
+		Fori(N) {
+			for (int j = i + 1; j < N; j++) {
+				if ((X[i] - X[j]) * (Y[i] - Y[j]) < 0) ans++;
+			}
+		}
+
+		out "Case #" << t << ": " << ans endl;
+	}
+}
