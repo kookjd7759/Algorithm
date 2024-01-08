1334dc78c443f86feb687ee5c27e103fc37e37b1
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-01-08 09:51:51 +0900

    [Bronze II] Title: Gold Coins, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Bronze/4864. Gold Coins/Gold Coins.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int ret[10001]{ 0, };
+
+int main() {
+	Sync;
+	
+	int coin(1), roop(1), roopmem(1);
+	For1i(10000) {
+		if (roop > 0) ret[i] = coin + ret[i - 1];
+		else {
+			coin++;
+			ret[i] = coin + ret[i - 1];
+			roopmem++;
+			roop = roopmem;
+		}
+		roop--;
+	}
+
+	while (true) {
+		int n; in n;
+		if (!n) break;
+		out n spc ret[n] endl;
+	}
+}
