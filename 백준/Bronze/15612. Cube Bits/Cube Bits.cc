23551a9455e5745c87b715c6a920e5af3ac70c2b
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-01-09 11:53:39 +0900

    [Bronze II] Title: Cube Bits, Time: 400 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15612. Cube Bits/Cube Bits.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+	 
+	Testcase{
+	ll n; in n;
+	if (!n) {
+		out 0 endl;
+		continue;
+	}
+	string ret = "";
+	while (n > 0) {
+		int temp = n % 3;
+		ret += (char)(temp + '0');
+		n /= 3;
+	}
+	reverse(ret.begin(), ret.end());
+	while (ret[0] == '0') {
+		ret.erase(0);
+	}
+	out ret endl;
+	};
+}
