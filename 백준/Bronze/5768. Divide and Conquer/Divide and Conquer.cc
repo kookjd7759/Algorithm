eeabbe6eff87338dd6b85e2b83adcf4791f7638a
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-01-09 10:26:05 +0900

    [Bronze II] Title: Divide and Conquer, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5768. Divide and Conquer/Divide and Conquer.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+    while (true) {
+        int a, b; cin >> a >> b;
+        if (a == 0 && b == 0) break;
+
+        int maxi(0), ans;
+        for (int i = a; i <= b; i++) {
+            int cnt(0);
+            For1j(i) if (i % j == 0) cnt++;
+
+            if (cnt >= maxi) {
+                maxi = cnt;
+                ans = i;
+            }
+        }
+
+        out ans spc maxi endl;
+    }
+}
