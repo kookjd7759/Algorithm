7d163dcffa69b04ba0cc35c4aba73f03ee9a73d3
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-01-11 14:54:28 +0900

    [Bronze II] Title: Crazy Fencing, Time: 4 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Bronze/21617. Crazy Fencing/Crazy Fencing.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+	Fixed(6);
+	
+	int n; in n;
+	double* h = new double[n + 1];
+	double* w = new double[n];
+	Fori(n + 1) in h[i];
+	Fori(n) in w[i];
+
+	double ret(0.0);
+	Fori(n) {
+		double mini = min(h[i], h[i + 1]);
+		ret += mini * w[i];
+		ret += abs(h[i] - h[i + 1]) * w[i] / 2.0;
+	}
+	out ret;
+}
