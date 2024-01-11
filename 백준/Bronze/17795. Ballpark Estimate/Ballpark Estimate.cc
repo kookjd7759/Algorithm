38ba01eef57444cf22e1500b375999b1d4af25ca
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-01-11 10:29:26 +0900

    [Bronze II] Title: Ballpark Estimate, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17795. Ballpark Estimate/Ballpark Estimate.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	string st; in st;
+	if (st.length() == 1) out st;
+	else {
+		if ((st[1] - '0') >= 5) {
+			if (st[0] != '9') st[0] = st[0] + 1;
+			else {
+				st = '1' + st;
+			}
+		}
+
+		out st[0];
+		For1i(st.size() - 1) out 0;
+	}
+}
