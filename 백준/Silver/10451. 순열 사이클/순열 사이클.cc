59adf61a3d87e1b49e601f57edfd8b949ff80d69
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-01-16 09:35:58 +0900

    [Silver III] Title: 순열 사이클, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	bool visited[1001];
+	int arr[1001];
+	Testcase{
+	memset(visited, false, sizeof visited);
+	int n; in n;
+	For1i(n) in arr[i];
+
+	int idx(1), cnt(0);
+	while (idx <= n) {
+		if (visited[idx]) idx++;
+		else {
+			cnt++;
+			while (!visited[idx]) {
+				visited[idx] = true;
+				idx = arr[idx];
+			}
+		}
+	}
+	out cnt endl;
+	}
+}
