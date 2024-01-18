125895c24257f67fb53eda2c6b1e6de25604dd05
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-01-18 09:51:43 +0900

    [Bronze II] Title: Лестница, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29141. Лестница/Лестница.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	int x1, y1, x2, y2, a, b;
+	in x1 >> y1 >> x2 >> y2 >> a >> b;
+	int x = abs(x1 - x2);
+	int y = abs(y1 - y2);
+
+	if (x / a >= (y + (b - 1)) / b) {
+		out x spc x / a endl;
+		out y spc x / a;
+	}
+	else out - 1;
+}
