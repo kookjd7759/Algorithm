b730fda53b3a17a1759522e24a7df2d12dcb4702
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-01-24 09:21:09 +0900

    [Bronze II] Title: Drawn and Quartered, Time: 0 ms, Memory: 2392 KB -BaekjoonHub

---
+++ 백준/Bronze/16103. Drawn and Quartered/Drawn and Quartered.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    ll n, k;
+    string st;
+    in n >> k >> st;
+    k %= 3;
+    vector<string> vec;
+    Fori(4) vec.emplace_back(st.substr(i * (n / 4), n / 4));
+    out vec[0];
+    Fori(3) out vec[(3 + i - k) % 3 + 1];
+}
