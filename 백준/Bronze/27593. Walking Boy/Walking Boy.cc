dd468301c49ed464dff163d4a63c5f246a963c0d
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-01-25 14:23:02 +0900

    [Bronze II] Title: Walking Boy, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27593. Walking Boy/Walking Boy.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+	
+	Testcase{
+	int n, prev(0), cnt(0); in n;
+	Fori(n) {
+		int t; in t;
+		if (t - prev >= 120) cnt += ((t - prev) / 120);
+		prev = t;
+	}
+
+	if (prev < 1320) cnt += ((1440 - prev) / 120);
+	if (cnt >= 2) out "YES\n";
+	else out "NO\n";
+	};
+}
