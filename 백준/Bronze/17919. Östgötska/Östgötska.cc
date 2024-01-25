4a669156127283299636606b2df97e046a42d4c9
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-01-25 10:13:25 +0900

    [Bronze II] Title: Östgötska, Time: 0 ms, Memory: 2076 KB -BaekjoonHub

---
+++ 백준/Bronze/17919. Östgötska/Östgötska.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+    
+    int cnt(0), aec(0);
+    string line, t; instr(line);
+    stringstream st;
+    st.str(line);
+    while (st >> t) {
+        if (t.find("ae") != string::npos) aec++;
+        cnt++;
+    }
+    cnt *= 100, aec *= 100;
+    if (cnt / 10 * 4 <= aec) out "dae ae ju traeligt va";
+    else out "haer talar vi rikssvenska";
+}
