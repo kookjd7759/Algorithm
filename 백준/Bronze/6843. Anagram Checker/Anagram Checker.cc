ee450c3d64eb9b2b5e864070885bf719450f7ebf
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-01-25 14:16:18 +0900

    [Bronze II] Title: Anagram Checker, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/6843. Anagram Checker/Anagram Checker.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+	
+	string a, b; 
+	instr(a); instr(b);
+	int ca[26], cb[26];
+	memset(ca, 0, sizeof ca);
+	memset(cb, 0, sizeof cb);
+
+	Fori(a.size()) ca[a[i] - 'A']++;
+	Fori(b.size()) cb[b[i] - 'A']++;
+	Fori(26) {
+		if (ca[i] != cb[i]) {
+			out "Is not an anagram.";
+			return 0;
+		}
+	}
+	out "Is an anagram.";
+}
