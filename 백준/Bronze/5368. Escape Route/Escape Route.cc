5a9cce541fc2f65de66b495ae0e4fc0d698538ab
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-01-25 15:19:06 +0900

    [Bronze II] Title: Escape Route, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/5368. Escape Route/Escape Route.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+double sx, sy, lx, ly, d;
+double gd(double a, double b) {
+	return sqrt(abs((sx - a) * (sx - a) + (sy - b) * (sy - b)));
+}
+
+int main() {
+    Sync;
+	Fixed(2);
+
+	Testcase{
+	int n; in n;
+	double mini = 0x3f3f3f3f;
+	vector<pair<double, double>> vec;
+	Fori(n) Forj(n) {
+		char c; in c;
+		if (c == 's') {
+			sx = i;
+			sy = j;
+		}
+		if (c == 'p') vec.push_back({ i, j });
+	}
+	for (const auto iter : vec) {
+		d = gd(iter.first, iter.second);
+		if (d < mini) {
+			lx = iter.first;
+			ly = iter.second;
+			mini = d;
+		}
+	}
+	out "(" << (int)sx << "," << (int)sy << "):(" <<
+		(int)lx << "," << (int)ly << "):" << mini endl;
+	};
+}
