40241ad0f015544f205fb90fe76af8bad96581d9
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-01-25 16:39:45 +0900

    [Bronze II] Title: Figury, Time: 60 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Bronze/8676. Figury/Figury.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int n; in n;
+    vector<int> vec(n);
+    Fori(n) in vec[i];
+
+    bool ch = false;
+
+    for (int i = 2; i < n; i++)
+        if (vec[i] != vec[i - 1] && vec[i - 1] != vec[i - 2] && 
+            vec[i - 2] != vec[i]) 
+            ch = true;
+
+    if (ch) out "TAK\n";
+    else out "NIE\n";
+}
