6ed751579641f574b477b9042d77e171ef455663
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-01-29 16:03:03 +0900

    [Bronze II] Title: Konsonantkoll, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/20817. Konsonantkoll/Konsonantkoll.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    string eng = "bcdfghjklmnpqrstvwxz";
+    string st; instr(st);
+    char prev = ' ';
+    int cnt(1);
+    Fori(st.size()) {
+        if (eng.find(st[i]) != string::npos) {
+            if (prev == st[i]) cnt++;
+            else cnt = 1;
+        }
+        else cnt = 1;
+
+        if (cnt < 3) out st[i];
+        prev = st[i];
+    }
+}
