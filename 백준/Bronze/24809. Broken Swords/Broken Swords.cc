ed3d8b9f2d015388dfeca06369bdce78137e47a0
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-01-31 13:58:02 +0900

    [Bronze II] Title: Broken Swords, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/24809. Broken Swords/Broken Swords.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+	
+	int t(0), b(0), tb;
+	Testcase{
+	string st; in st;
+	if (st[0] == '0') t++;
+	if (st[1] == '0') t++;
+	if (st[2] == '0') b++;
+	if (st[3] == '0') b++;
+	tb = min(t, b) / 2;
+	}
+	out tb spc t - tb * 2 spc b - tb * 2;
+}
