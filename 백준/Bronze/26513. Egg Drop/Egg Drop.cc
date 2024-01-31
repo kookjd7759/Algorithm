6a23fbbfb33bc7e835febb21d34bbf548ebdb6cc
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-01-31 13:39:53 +0900

    [Bronze II] Title: Egg Drop, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/26513. Egg Drop/Egg Drop.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+	
+	while (true) {
+		int n, k; in n >> k;
+		if (!n && !k) break;
+
+		int mini(1), maxi(k);
+		Fori(n) {
+			int d; string st; in d >> st;
+			if (st[0] == 'S') mini = max(mini, d);
+			else maxi = min(maxi, d);
+		}
+		out mini + 1 spc maxi - 1 endl;
+	}
+}
