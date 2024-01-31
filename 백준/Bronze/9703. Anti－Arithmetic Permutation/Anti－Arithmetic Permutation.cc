4dd6aa634d024fa4491a71237fbc823b192d6aa0
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-01-31 14:19:50 +0900

    [Bronze II] Title: Anti-Arithmetic Permutation, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Bronze/9703. Anti－Arithmetic Permutation/Anti－Arithmetic Permutation.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int T; in T;
+    for (int tc = 1; tc <= T; tc++) {
+        int n; in n;
+        int* arr = new int[n];
+        Fori(n) in arr[i];
+
+        bool ch = false;
+        for (int i = 0; i < n - 2; i++)
+            for (int j = i + 1; j < n - 1; j++)
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[i] - arr[j] == arr[j] - arr[k]) {
+                        ch = true;
+                        break;
+                    }
+                }
+
+        out "Case #" << tc << ": " << (!ch ? "YES" : "NO") endl;
+    }
+}
