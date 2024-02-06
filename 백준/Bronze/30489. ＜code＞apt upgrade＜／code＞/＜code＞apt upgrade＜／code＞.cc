ff510143811021c20a2d8fb1ce14f08a962b4261
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-02-06 09:13:40 +0900

    [Bronze II] Title: <code>apt upgrade</code>, Time: 36 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Bronze/30489. ＜code＞apt upgrade＜／code＞/＜code＞apt upgrade＜／code＞.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+    Fixed(6);
+
+    int n, m, k; in n >> m >> k;
+    double sum(0.0), d(0.0);
+    vector<double> vec;
+    Fori(n) {
+        double a; in a;
+        sum += a;
+        vec.emplace_back(a);
+    }
+    sort(vec.begin(), vec.end(), greater<>());
+    m += k;
+    Fori(m) d += vec[i];
+    out d / sum * 100.0;
+}
