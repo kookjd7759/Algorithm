2a735c2a0fb48d27ae619cc953feb5b72825788f
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-02-13 10:57:04 +0900

    [Bronze II] Title: Finalists, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/26310. Finalists/Finalists.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+double m[5] = { 0.56, 0.24, 0.14, 0.06, 0.3 };
+
+int main() {
+    Sync;
+
+    int n; in n;
+    vector<pair<double, string>> vec;
+    Fori(6) {
+        string st; in st;
+        double t, ss(0);
+        Forj(5) {
+            double t; in t;
+            ss += t * m[j];
+        }
+        vec.push_back({ ss, st });
+    }
+    sort(vec.begin(), vec.end(), greater<>());
+    int sl[6]; memset(sl, 0, sizeof sl);
+    Fori(n) sl[i % 6]++;
+    Fori(6) if (vec[i].second == "Taiwan") {
+        out sl[i];
+        break;
+    }
+}
