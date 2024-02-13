e11249e387f7adb6c6488d1182a5da0f9247c8e3
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-02-13 10:36:39 +0900

    [Bronze II] Title: SD КАРТИ, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24334. SD КАРТИ/SD КАРТИ.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+    Fixed(2);
+
+    int n, m(0); in n;
+    Fori(n) {
+        int a, b; in a >> b;
+        m += 60 * a + b;
+    }
+    double mini = 0x3f3f3f3f;
+    for (int i = 50; i >= 0; i--) {
+        for (int j = 50; j >= 0; j--)
+            if (i * 240 + j * 180 >= m) mini = min(mini, i * 10.9 + j * 9.15);
+    }
+
+    out mini;
+}
