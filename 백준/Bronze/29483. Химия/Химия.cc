8a74cfa2872021bdd2b05ddf15d1bd4516b07d30
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-02-14 09:21:54 +0900

    [Bronze II] Title: Химия, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/29483. Химия/Химия.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int ret(0), temp(0);
+    string st; in st;
+    Fori(st.size()) {
+        if (isupper(st[i]) && temp != 0) ret += temp, temp = 0;
+
+        if (st[i] == 'H') temp = 1;
+        else if (st[i] == 'C') temp = 12;
+        else if (st[i] == 'N') temp = 14;
+        else if (st[i] == 'O') temp = 16;
+        else {
+            temp *= (st[i] - '0');
+            ret += temp;
+            temp = 0;
+        }
+    }
+    out ret + temp;
+}
