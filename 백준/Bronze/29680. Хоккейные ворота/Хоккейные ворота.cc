cfa7ff2ba1df6523f63439f1193a6c8edfe2a951
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-02-14 09:17:26 +0900

    [Bronze II] Title: Хоккейные ворота, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/29680. Хоккейные ворота/Хоккейные ворота.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+    Sync;
+    Fixed(6);
+
+    double H, W, d1, d2; in H >> W >> d1 >> d2;
+    double ret = d1 * W;
+    ret += (d1 * H + (d2 - d1) * H / 2) * 2;
+    double backLen = sqrt((d2 - d1) * (d2 - d1) + H * H);
+    ret += backLen * W;
+    out ret;
+}
