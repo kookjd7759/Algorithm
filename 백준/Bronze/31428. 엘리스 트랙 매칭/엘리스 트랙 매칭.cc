9e27bdf1d78e9bb316a8e38033fd5a4c0c8fbd93
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-02-20 09:21:17 +0900

    [Bronze IV] Title: 엘리스 트랙 매칭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31428. 엘리스 트랙 매칭/엘리스 트랙 매칭.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	int CSIA[4]{ 0,0,0,0 };
+	auto get = [&](char c) -> int* {
+		if (c == 'C') return &CSIA[0];
+		else if (c == 'S') return &CSIA[1];
+		else if (c == 'I') return &CSIA[2];
+		else return &CSIA[3];
+		};
+	int n; in n;
+	Fori(n) {
+		char c; in c;
+		int* temp = get(c);
+		*temp += 1;
+	}
+	char c; in c;
+	out* get(c);
+}
