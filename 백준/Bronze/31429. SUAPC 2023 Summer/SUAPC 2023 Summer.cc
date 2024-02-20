5e885449b1f15f0e03ab43885557a40443d1d7bc
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-02-20 09:12:22 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	int ret[2][11]{
+		12,11,11,10,9,9,9,8,7,6,6,
+		1600,894,1327,1311,1004,1178,1357,837,1055,556,773
+	};
+
+	int n; in n; n--;
+	out ret[0][n] spc ret[1][n];
+}
