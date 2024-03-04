645962dedec5c17f2e0089525844cabe2862bc95
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-03-04 10:40:18 +0900

    [Bronze II] Title: 생일 축하합니다~, Time: 12 ms, Memory: 30356 KB -BaekjoonHub

---
+++ 백준/Bronze/31497. 생일 축하합니다～/생일 축하합니다～.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+	Interactive;
+
+	int n; in n;
+	string* list = new string[n];
+	Fori(n) in list[i];
+	Fori(n) {
+		bool bir = false;
+		Forj(2) {
+			bool ans;
+			out '?' spc list[i] endl << flush;
+			in ans;
+			bir ^= ans;
+		}
+		if (bir) {
+			out '!' spc list[i];
+			break;
+		}
+	}
+}
