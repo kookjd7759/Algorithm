88c3a20db8aa7a3f1108c82c3b4b9feb105fcc56
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-03-04 10:01:20 +0900

    [Bronze II] Title: !!초콜릿 중독 주의!!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/31458. ！！초콜릿 중독 주의！！/！！초콜릿 중독 주의！！.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+	
+	int T; in T; 
+	while (T--) {
+		int Not(0), Fac(0), n(0);
+		bool front = true;
+		string st; in st;
+		Fori(st.size()) {
+			if (st[i] == '!') {
+				if (front) Not++;
+				else Fac++;
+			}
+			else {
+				n = st[i] - '0';
+				front = false;
+			}
+		}
+
+		if (n == 0 && Fac >= 1) n = 1;
+		if (Not & 1) n = n == 0 ? 1 : 0;
+
+		out n endl;
+	}
+}
