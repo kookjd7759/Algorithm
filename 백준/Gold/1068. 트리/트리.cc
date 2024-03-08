971ab868de7b847191104c149654aee6aabd8c18
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-03-08 10:22:55 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1068. 트리/트리.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	int n, root(-1); in n;
+	vector<vector<int>> tree(n);
+	Fori(n) {
+		int a; in a;
+		if (a == -1) root = i;
+		else tree[a].push_back(i);
+	}
+	int del; in del;
+	stack<int> st; st.push(root);
+	int cnt(0);
+	while (!st.empty()) {
+		int node = st.top(); st.pop();
+		if (node == del) continue;
+		if (tree[node].size() == 0) cnt++;
+		else if (tree[node].size() == 1 && tree[node][0] == del) cnt++;
+		Fori(tree[node].size()) st.push(tree[node][i]);
+	}
+	out cnt;
+}
