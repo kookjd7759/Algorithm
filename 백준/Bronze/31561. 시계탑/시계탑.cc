aa5ac6bb5e53a8788235e75a3a9309ab58f763af
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-03-11 09:10:11 +0900

    [Bronze IV] Title: 시계탑, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31561. 시계탑/시계탑.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+	Fixed(1);
+
+	double m, a, b; in m;
+	a = m, b = m - 30.0;
+	if (a > 30.0) a = 30.0;
+	if (b < 0.0) b = 0;
+	out a * 0.5 + b * 1.5;
+}
