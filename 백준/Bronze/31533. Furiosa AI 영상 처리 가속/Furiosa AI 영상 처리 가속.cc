eaf285c89249656014de8704dbc4232a9d02910b
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-03-12 09:24:55 +0900

    [Bronze II] Title: Furiosa AI 영상 처리 가속, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31533. Furiosa AI 영상 처리 가속/Furiosa AI 영상 처리 가속.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+	Fixed(10);
+
+	double a, m, n, ret = 0x3f3f3f3f; in a >> m >> n;
+	ret = min(ret, m * 2.0 / a);
+	ret = min(ret, n * 2.0 / a);
+	double temp = max(max(m / a, n), max(m, n / a));
+	ret = min(ret, temp);
+	out ret;
+}
