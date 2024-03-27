6cac5093a90ecd481567d429263fe6f304246f3f
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-03-27 09:07:16 +0900

    [Bronze III] Title: 差 (Difference), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31617. 差 （Difference）/差 （Difference）.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	int k; in k;
+	int n; in n;
+	int* A = new int[n];
+	Fori(n) in A[i];
+	int m; in m;
+	int* B = new int[m];
+	Fori(m) in B[i];
+
+	int cnt(0);
+	Fori(n) Forj(m) {
+		if (A[i] + k == B[j]) cnt++;
+	}
+	out cnt;
+}
