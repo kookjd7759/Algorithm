7c090f1fad28d44545739f9e846ec9892471667f
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-03-27 09:28:13 +0900

    [Bronze III] Title: Mean Words, Time: 0 ms, Memory: 2172 KB -BaekjoonHub

---
+++ 백준/Bronze/31663. Mean Words/Mean Words.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define Testcase int tc; in tc; while(tc--) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	int n, len(0); in n;
+	string* st = new string[n];
+	Fori(n) {
+		in st[i];
+		int size = st[i].size();
+		len = max(len, size);
+	}
+
+	Fori(len) {
+		int sum(0), cnt(0);
+		Forj(n) {
+			if (i < st[j].size())
+				sum += st[j][i], cnt++;
+		}
+		out(char)(sum / cnt);
+	}
+}
