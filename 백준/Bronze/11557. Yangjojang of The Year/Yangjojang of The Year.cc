4c0bf4eaad8d69214e8f6ad2d8af81c03f8b5ddf
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-04-22 09:04:47 +0900

    [Bronze I] Title: Yangjojang of The Year, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define nonecountingTC int tc; in tc; while(tc--) 
+#define countingTC int tc; in tc; for (int TCcounter = 1; TCcounter <= tc; TCcounter++) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	nonecountingTC{
+
+	int n, maxi(-2e9); in n;
+	string ret = "";
+	Fori(n) {
+		string name; in name;
+		int num; in num;
+		if (num > maxi) {
+			maxi = num;
+			ret = name;
+		}
+	}
+	out ret endl;
+
+	};
+}
