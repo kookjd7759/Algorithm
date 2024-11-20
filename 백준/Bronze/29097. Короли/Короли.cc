339e873f58c8d98830e8e0a8fb6453ef6d9f3a94
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-11-20 09:35:15 +0900

    [Bronze IV] Title: Короли, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Bronze/29097. Короли/Короли.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define nonecountingTC int tc; in tc; while(tc--) 
+#define countingTC int tc; in tc; for (int TCcounter = 1; TCcounter <= tc; TCcounter++) 
+
+using namespace std;
+
+struct Node {
+	int score;
+	string name;
+	Node(int s, string n) : score(s), name(n){}
+};
+
+bool comp(const Node& a, const Node& b) {
+	if (a.score == b.score) return a.name < b.name;
+	else return a.score > b.score;
+}
+
+int main() {
+	Sync;
+
+	int n, m, k, a, b, c; in n >> m >> k >> a >> b >> c;
+	vector<Node> vec{ Node(n * a, "Joffrey"), Node(m * b, "Robb"), Node(k * c, "Stannis") };
+	sort(vec.begin(), vec.end(), comp);
+	int max = vec[0].score;
+	Fori(3) {
+		if (max == vec[i].score) out vec[i].name << ' ';
+	}
+}
