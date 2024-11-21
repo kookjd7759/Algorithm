4310630a6425e2483bef385db9a67821d7636c89
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-11-21 09:14:59 +0900

    [Bronze IV] Title: Juta teekond, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32722. Juta teekond/Juta teekond.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define nonecountingTC int tc; in tc; while(tc--) 
+#define countingTC int tc; in tc; for (int TCcounter = 1; TCcounter <= tc; TCcounter++) 
+
+using namespace std;
+
+bool check(int n, int a, int b) {
+	return (n == a || n == b);
+}
+
+int main() {
+	Sync;
+
+	int a, b, c; in a >> b >> c;
+	if (check(a, 1, 3) && check(b, 6, 8) && check(c, 2, 5))
+		out "JAH";
+	else out "EI";
+}
