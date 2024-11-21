3ebd5da7aeada14745204efc61d1870f1b288a26
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-11-21 09:26:49 +0900

    [Bronze IV] Title: 部員の変遷, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Bronze/32089. 部員の変遷/部員の変遷.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define nonecountingTC int tc; in tc; while(tc--) 
+#define countingTC int tc; in tc; for (int TCcounter = 1; TCcounter <= tc; TCcounter++) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	while (true) {
+		int n; in n;
+		if (!n) break;
+		int* arr = new int[n];
+		Fori(n) in arr[i];
+		
+		int maxi(0);
+		Fori(n - 2) {
+			int sum = arr[i] + arr[i + 1] + arr[i + 2];
+			maxi = max(maxi, sum);
+		}
+		out maxi endl;
+	}
+}
