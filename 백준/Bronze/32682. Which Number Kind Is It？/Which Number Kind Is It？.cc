da6362f0a1163762c977d686b83cbc5accc028c3
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-11-22 09:49:38 +0900

    [Bronze III] Title: Which Number Kind Is It?, Time: 12 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/32682. Which Number Kind Is It？/Which Number Kind Is It？.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; i++)
+#define Forj(x) for (int j = 0; j < x; j++)
+#define Fork(x) for (int k = 0; k < x; k++)
+#define For1i(x) for (int i = 1; i <= x; i++)
+#define For1j(x) for (int j = 1; j <= x; j++)
+#define For1k(x) for (int k = 1; k <= x; k++)
+#define nonecountingTC int tc; in tc; while(tc--) 
+#define countingTC int tc; in tc; for (int TCcounter = 1; TCcounter <= tc; TCcounter++) 
+
+using namespace std;
+
+int main() {
+	Sync;
+
+	nonecountingTC{
+
+	int n; in n;
+	bool inc = false;
+	if (n % 2 == 1) out 'O', inc = true;
+	int s = sqrt(n);
+	if (s * s == n) out 'S', inc = true;
+	if (!inc) out "EMPTY";
+	ent;
+
+	}
+}
