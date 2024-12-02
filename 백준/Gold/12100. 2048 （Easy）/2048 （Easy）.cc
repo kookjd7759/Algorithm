2e9798ebde7ecbf869458681054272bf2b8a20c0
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-12-02 13:11:37 +0900

    [Gold I] Title: 2048 (Easy), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
@@ -21,168 +21,82 @@
 
 using namespace std;
 
-enum Dir {
-	UP, LEFT, RIGHT, DOWN,
-	DIR_NUM, DIR_NULL
-};
-Dir& operator++(Dir& d) { d = (Dir)(d + 1); return d; }
-constexpr inline Dir opposite(const Dir& d) {
-	if (d == UP) return DOWN;
-	else if (d == LEFT) return RIGHT;
-	else if (d == RIGHT) return LEFT;
-	else if (d == DOWN) return UP;
-	else return DIR_NULL;
+int SIZE, ans;
+int board[20][20];
+
+void init() { in SIZE; Fori(SIZE) Forj(SIZE) in board[i][j]; }
+void rotation(int temp[20][20]) { Fori(SIZE) Forj(SIZE) board[SIZE - j - 1][i] = temp[i][j]; }
+void update_maxi() { Fori(SIZE) Forj(SIZE) ans = max(ans, board[i][j]); }
+
+void move() {
+	Fori(SIZE) {
+		int dest = 0;
+		For1j(SIZE + 1) {
+			if (!board[i][j]) continue;
+
+			if (!board[i][dest]) board[i][dest] = board[i][j];
+			else {
+				if (board[i][dest] & board[i][j]) {
+					board[i][dest] <<= 1;
+					dest++;
+				}
+				else {
+					dest++;
+					if (dest == j) continue;
+					board[i][dest] = board[i][j];
+				}
+			}
+			board[i][j] = 0;
+		}
+	}
 }
 
-struct Board {
-	int** board = nullptr;
-	int size = 0, maxi = 0;
-	Dir prevMove = DIR_NULL;
-	bool prevJoin = false;
-
-	void init() {
-		in size;
-		board = new int* [size];
-		Fori(size) board[i] = new int[size];
-		Fori(size) Forj(size) in board[i][j], maxi = max(maxi, board[i][j]);
-	}
-	Board() {}
-	Board(const Board& other) {
-		size = other.size;
-		maxi = other.maxi;
-		board = new int* [size];
-		Fori(size) board[i] = new int[size];
-		Fori(size) Forj(size) board[i][j] = other.board[i][j];
+void DFS(int cnt) {
+	if (!cnt) {
+		update_maxi();
+		return;
 	}
 
-	void moveLine(Dir dir, int idx) {
-		int dest, cnt(0);
-		bool join = false;
-		if (dir == LEFT) {
-			dest = 0;
-			for (int i = 1; i < size; i++) {
-				if (!board[idx][i]) continue;
-
-				if (!board[idx][dest]) board[idx][dest] = board[idx][i];
-				else {
-					if (board[idx][dest] & board[idx][i]) {
-						board[idx][dest] <<= 1;
-						maxi = max(maxi, board[idx][dest]);
-						dest++;
-						join = true;
+	Fori(4) {
+		int temp[20][20]; memcpy(temp, board, sizeof(board));
+		for (int row = 0; row < SIZE; row++) {
+			int loc = 0, ret = 0;
+			bool flag = false;
+			for (int i = 0; i < SIZE; i++) {
+				if (board[row][i] != 0) {
+					if (board[row][i] == board[row][loc] && (i != 0 && flag)) {
+						board[row][loc] += board[row][loc];
+						board[row][i] = 0;
+						flag = false;
+						loc++;
 					}
 					else {
-						dest++;
-						if (dest == i) continue;
-						board[idx][dest] = board[idx][i];
+						if (flag)
+							loc++;
+						ret = board[row][i];
+						board[row][i] = 0;
+						board[row][loc] = ret;
+						flag = true;
 					}
 				}
-				board[idx][i] = 0;
 			}
 		}
-		else if (dir == RIGHT) {
-			dest = size - 1;
-			for (int i = size - 2; i >= 0; i--) {
-				if (!board[idx][i]) continue;
-
-				if (!board[idx][dest]) board[idx][dest] = board[idx][i];
-				else {
-					if (board[idx][dest] & board[idx][i]) {
-						board[idx][dest] <<= 1;
-						maxi = max(maxi, board[idx][dest]);
-						dest--;
-						join = true;
-					}
-					else {
-						dest--;
-						if (dest == i) continue;
-						board[idx][dest] = board[idx][i];
-					}
-				}
-				board[idx][i] = 0;
-			}
-		}
-		else if (dir == UP) {
-			dest = 0;
-			for (int i = 1; i < size; i++) {
-				if (!board[i][idx]) continue;
-
-				if (!board[dest][idx]) board[dest][idx] = board[i][idx];
-				else {
-					if (board[dest][idx] & board[i][idx]) {
-						board[dest][idx] <<= 1;
-						maxi = max(maxi, board[dest][idx]);
-						dest++;
-						join = true;
-					}
-					else {
-						dest++;
-						if (dest == i) continue;
-						board[dest][idx] = board[i][idx];
-					}
-				}
-				board[i][idx] = 0;
-			}
-		}
-		else {
-			dest = size - 1;
-			for (int i = size - 2; i >= 0; i--) {
-				if (!board[i][idx]) continue;
-
-				if (!board[dest][idx]) board[dest][idx] = board[i][idx];
-				else {
-					if (board[dest][idx] & board[i][idx]) {
-						board[dest][idx] <<= 1;
-						maxi = max(maxi, board[dest][idx]);
-						dest--;
-						join = true;
-					}
-					else {
-						dest--;
-						if (dest == i) continue;
-						board[dest][idx] = board[i][idx];
-					}
-				}
-				board[i][idx] = 0;
-			}
-		}
-
-		prevJoin = join;
+		DFS(cnt - 1);
+		rotation(temp);
 	}
-	void move(Dir dir) {
-		Fori(size) moveLine(dir, i);
+}
 
-		prevMove = dir;
-	}
-
-	bool operator==(const Board& other) const {
-		Fori(size) Forj(size) if (board[i][j] != other.board[i][j]) return false;
-		return true;
-	}
-};
-
-int maxi(0);
-
-void DFS(Board& board, int cnt) {
-	maxi = max(maxi, board.maxi);
-
-	if (!cnt) return;
-
-	for (Dir dir = UP; dir < DIR_NUM; ++dir) {
-		if (!board.prevJoin && opposite(board.prevMove) == dir) continue;
-		Board next(board);
-		next.move(dir);
-		if (board == next) continue;
-		DFS(next, cnt - 1);
+void print(int arr[20][20]) {
+	Fori(SIZE) {
+		Forj(SIZE) out board[i][j] << ' ';
+		ent;
 	}
 }
 
 int main() {
 	Sync;
 
-	int SIZE = 5;
-
-	Board board; board.init();
-	DFS(board, SIZE);
-	out maxi;
+	init();
+	DFS(5);
+	out ans;
 }
