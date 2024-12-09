89bfaeb5ea84c8739bb6281ebd77da59703b0d7f
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-12-09 10:46:18 +0900

    [Bronze III] Title: 보물 찾기, Time: 4 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/32068. 보물 찾기/보물 찾기.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+#define nonecountingTC int tc; in tc; while(tc--) 
+#define countingTC int tc; in tc; for (int TCcounter = 1; TCcounter <= tc; TCcounter++) 
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    nonecountingTC{
+
+    int t[2], pos; in t[0] >> t[1] >> pos;
+    int nearIdx = (abs(pos - t[0]) >= abs(pos - t[1]) ? 1 : 0);
+    int level(1);
+
+    if (nearIdx == 1) level += abs(pos - t[1]) * 2 - 1;
+    else level += abs(pos - t[0]) * 2;
+
+    out level endl;
+
+    }
+}
