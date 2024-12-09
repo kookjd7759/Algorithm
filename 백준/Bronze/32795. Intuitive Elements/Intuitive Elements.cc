08208389b67e163846675cb4030d1d2780655f5f
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2024-12-09 13:28:11 +0900

    [Bronze III] Title: Intuitive Elements, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/32795. Intuitive Elements/Intuitive Elements.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define instr(x) getline(cin, x)
+#define ll long long
+#define in cin >>
+#define out cout <<
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int n; in n;
+    Fori(n) {
+        string name, st; in name >> st;
+        uint32_t name_bit(0), st_bit(0);
+        Fori(name.size()) name_bit |= (1 << (name[i] - 'a'));
+        Fori(st.size()) st_bit |= (1 << (st[i] - 'a'));
+        
+        out((st_bit & name_bit) == st_bit ? "YES" : "NO") endl;
+    }
+}
