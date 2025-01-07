11d5194789323a488d42ad8e69fa275d898a76a6
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2025-01-07 10:58:02 +0900

    [Bronze II] Title: Öar, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20811. Öar/Öar.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout << 
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int n, ans(2), sum(2); in n;
+    int arr[2] = { 1, 1 };
+    if (n <= 2) out n;
+    else {
+        while (sum < n) {
+            int add = arr[0] + arr[1];
+            arr[0] = arr[1];
+            arr[1] = add;
+            sum += add;
+            ++ans;
+        }
+        out ans;
+    }
+}
