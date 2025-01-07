f447cfce160d4e6539ba3d8527295cb4ed5a40ef
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2025-01-07 10:29:14 +0900

    [Bronze II] Title: Alpha Puzzle, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/13776. Alpha Puzzle/Alpha Puzzle.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout << 
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int n; in n; cin.ignore();
+    bool alpha[26]; memset(alpha, false, sizeof(alpha));
+    string* line = new string[n], ret("");
+    Fori(n) {
+        getline(cin, line[i]);
+        while (line[i].size() < n) line[i] += ' ';
+    }
+
+    Fori(n) Forj(n) {
+        if (isupper(line[i][j])) {
+            int idx = line[i][j] - 'A';
+            if (!alpha[idx]) {
+                ret += line[i][j];
+                alpha[idx] = true;
+            }
+        }
+
+        if (ret.size() == 26) break;
+    }
+    out ret;
+}
