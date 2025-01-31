3792cd2f8a8198ca12c50afa1423e9b78df5e48d
Donggyun_7759 <67672017+kookjd7759@users.noreply.github.com>
2025-01-31 09:27:52 +0900

    [Bronze III] Title: 국방시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33277. 국방시계/국방시계.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout << 
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int total, now; in total >> now;
+    int min = (double)(24 * 60 * 60) * ((double)now / (double)total) / 60;
+    int h = min / 60, m = min % 60;
+    if (h < 10) out 0;
+    out h << ':';
+    if (m < 10) out 0;
+    out m;
+}
