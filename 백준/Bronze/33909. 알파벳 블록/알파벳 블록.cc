17d99fa876b56535d4ebca75ff37d5f4e2db9906
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-05-21 14:20:28 +0900

    [Bronze III] Title: 알파벳 블록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33909. 알파벳 블록/알파벳 블록.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout << 
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+bool is_vowel(char c) { return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'; }
+
+int main() {
+    Sync;
+    // needs = { 2, 4, 1, 1 };
+    int scon[4]; Fori(4) in scon[i];
+    int sn_max((scon[0] + scon[3]) / 3);
+    int co_max((scon[1] + scon[2] * 2) / 6);
+    out min(sn_max, co_max);
+}
