37ea1d04f6afc78d5db8edc4fd09f8648cdb2af3
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-05-27 13:42:44 +0900

    [Silver V] Title: LCM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/5347. LCM/LCM.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout << 
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+ll gcd(ll a, ll b) {
+	while (b != 0) {
+		ll r = a % b;
+		a = b;
+		b = r;
+	}
+	return a;
+
+}
+int main() {
+    Sync;
+
+    int t; in t;
+    while (t--) {
+        ll a, b; in a >> b;
+		out a * b / gcd(a, b) endl;
+    }
+}
