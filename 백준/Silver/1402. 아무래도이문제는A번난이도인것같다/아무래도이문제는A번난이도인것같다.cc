4fea17595de0593056493e0406d1490819794bc3
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-05-27 13:39:26 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout << 
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int t; in t;
+    while (t--) {
+        int a, b; in a >> b;
+        out "yes" endl;
+    }
+}
