5153ba32983a8aceac9bc619a8111f40733a53c4
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-07-23 11:36:38 +0900

    [Bronze I] Title: 0의 개수, Time: 52 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11170. 0의 개수/0의 개수.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout << 
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+    
+    int n; in n;
+    Fori(n) {
+        int a, b, cnt(0); in a >> b;
+        if (a == 0) ++a, ++cnt;
+        for (int j = a; j <= b; ++j) {
+            int num = j;
+            while (num > 0) {
+                if (num % 10 == 0) cnt++;
+                num /= 10;
+            }
+        }
+        out cnt endl;
+    }
+}
