cf9bed238ddba2e841abda067c18344614acaed4
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-07-23 14:20:20 +0900

    [Bronze I] Title: 애너그램, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/6996. 애너그램/애너그램.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout << 
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+    
+    int tc; in tc;
+    while (tc--) {
+        int alphaA[26], alphaB[26];
+        memset(alphaA, 0, sizeof(alphaA));
+        memset(alphaB, 0, sizeof(alphaB));
+
+        string a, b; in a >> b;
+        Fori(a.size()) alphaA[a[i] - 'a']++;
+        Fori(b.size()) alphaB[b[i] - 'a']++;
+
+        bool isAna(true);
+        Fori(26) if (alphaA[i] != alphaB[i]) { isAna = false; break; }
+
+        out a << " & " << b << " are ";
+        out(isAna ? "anagrams." : "NOT anagrams.") endl;
+    }
+}
