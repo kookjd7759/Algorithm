c6c1aafcf89a3badeebf9de9b54f94a85a2d1d7b
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-10-21 14:32:32 +0900

    [Bronze V] Title: Above, Time: 180 ms, Memory: 17996 KB -BaekjoonHub

---
+++ 백준/Bronze/34362. Above/Above.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double a = sc.nextDouble();
+		System.out.print(a - 0.3);
+	}
+}
