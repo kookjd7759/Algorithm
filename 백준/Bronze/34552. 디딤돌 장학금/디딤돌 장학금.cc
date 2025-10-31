e013b4c463707dccf50794ed7bc437f90eba3ba5
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-10-31 10:50:28 +0900

    [Bronze IV] Title: 디딤돌 장학금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34552. 디딤돌 장학금/디딤돌 장학금.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+    int M[11];
+    Fori(11) in M[i];
+    int N; in N;
+    long long ans = 0;
+    Fori(N) {
+        int B, S;
+        double L;
+        in B >> L >> S;
+        if (S >= 17 && L >= 2.0) ans += M[B];
+    }
+    out ans;
+}
