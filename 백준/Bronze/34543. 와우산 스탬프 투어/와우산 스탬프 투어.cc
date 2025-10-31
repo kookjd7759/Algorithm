200dfdbab8a83b404edfd9fb6717818edf9ef277
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-10-31 10:48:05 +0900

    [Bronze IV] Title: 와우산 스탬프 투어, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34543. 와우산 스탬프 투어/와우산 스탬프 투어.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+    int N, W;
+    in N;
+    in W;
+    int score = N * 10;
+    if (N >= 3) score += 20;
+    if (N == 5) score += 50;
+    if (W > 1000) score -= 15;
+    if (score < 0) score = 0;
+    out score;
+}
