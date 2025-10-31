474d6519c85e9e346a0ee0129e94141d4e90c2d0
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-10-31 11:05:16 +0900

    [Bronze III] Title: Fair Grading, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34282. Fair Grading/Fair Grading.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+    int x, y, z;
+    in x >> y >> z;
+    double finalScore = x * 0.25 + y * 0.25 + z * 0.5;
+    if (finalScore >= 90) out 'A';
+    else if (finalScore >= 80) out 'B';
+    else if (finalScore >= 70) out 'C';
+    else if (finalScore >= 60) out 'D';
+    else out 'F';
+}
