32a057454035a43abec123fc27c5c121647f4c4b
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-11-07 13:42:10 +0900

    [Bronze I] Title: 오늘도 졌다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14582. 오늘도 졌다/오늘도 졌다.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int A[9], B[9];
+    Fori(9) in A[i];
+    Fori(9) in B[i];
+    int sa = 0, sb = 0;
+    bool lead = false;
+    Fori(9) {
+        sa += A[i];
+        if (sa > sb) lead = true;
+        sb += B[i];
+    }
+    out(lead ? "Yes" : "No") endl;
+}
