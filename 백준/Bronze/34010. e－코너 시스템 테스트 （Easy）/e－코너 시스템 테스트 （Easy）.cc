943031ba427b4524fd61a5e11258df2429091715
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-11-10 10:49:41 +0900

    [Bronze II] Title: e-코너 시스템 테스트 (Easy), Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34010. e－코너 시스템 테스트 （Easy）/e－코너 시스템 테스트 （Easy）.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    long long N; in N;
+    long long total = 2 * N * (N - 1);
+    Fori(total) { long long x; in x; }
+    long long dist = 2 * (N - 1);
+    long long piv = dist - 1;
+    out dist spc piv << "\n";
+    return 0;
+}
