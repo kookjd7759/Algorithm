e3f1616230a911c4b0a11902cfdc9d2eab6d52dd
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-11-10 10:42:01 +0900

    [Bronze II] Title: 초콜릿 우유가 좋아, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34510. 초콜릿 우유가 좋아/초콜릿 우유가 좋아.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    long long H1, H2, H3; in H1 >> H2 >> H3;
+    long long N; in N;
+    long long A = H1 + H2 + H3;
+    long long B = H3 - H1;
+    long long e = N / 2;
+    long long o = N - e;
+    out o* A + e * B << "\n";
+    return 0;
+}
