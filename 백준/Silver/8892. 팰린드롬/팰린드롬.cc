febc716b5340c96215b9e7b2ae9752422db4b343
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-11-13 10:43:09 +0900

    [Silver V] Title: 팰린드롬, Time: 28 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/8892. 팰린드롬/팰린드롬.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+bool isPal(const string& s) {
+    int l = 0, r = (int)s.size() - 1;
+    while (l < r) {
+        if (s[l] != s[r]) return false;
+        ++l; --r;
+    }
+    return true;
+}
+
+int main() {
+    Sync;
+
+    int T;
+    in T;
+    while (T--) {
+        int k;
+        in k;
+        vector<string> v(k);
+        Fori(k) in v[i];
+        bool found = false;
+        string ans;
+        Fori(k) {
+            Forj(k) {
+                if (i == j) continue;
+                string t = v[i] + v[j];
+                if (isPal(t)) {
+                    ans = t;
+                    found = true;
+                    break;
+                }
+            }
+            if (found) break;
+        }
+        if (found) out ans endl;
+        else out 0 endl;
+    }
+
+    return 0;
+}
