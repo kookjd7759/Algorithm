3edfed81ca11cc032d7de9623d96dc9e794b0c82
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-11-18 11:18:27 +0900

    [Silver V] Title: 속도 위반, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11971. 속도 위반/속도 위반.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >>
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int N, M;
+    in N >> M;
+
+    int limit[100], speed[100];
+    int pos = 0;
+    Fori(N) {
+        int len, s;
+        in len >> s;
+        Forj(len) limit[pos++] = s;
+    }
+
+    pos = 0;
+    Fori(M) {
+        int len, s;
+        in len >> s;
+        Forj(len) speed[pos++] = s;
+    }
+
+    int ans = 0;
+    Fori(100) {
+        int diff = speed[i] - limit[i];
+        if (diff > ans) ans = diff;
+    }
+
+    out ans;
+
+    return 0;
+}
