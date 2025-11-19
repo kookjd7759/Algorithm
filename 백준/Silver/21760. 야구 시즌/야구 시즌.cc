d163cdce1ad26088e826b2487b603e9d9737abb1
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-11-19 10:52:36 +0900

    [Silver V] Title: 야구 시즌, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/21760. 야구 시즌/야구 시즌.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >>
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int T;
+    in T;
+    while (T--) {
+        ll N, M, k, D;
+        in N >> M >> k >> D;
+
+        ll C1 = N * M * (M - 1) / 2;
+        ll C2 = N * (N - 1) / 2 * M * M;
+        ll coef = C1 * k + C2;
+
+        if (coef > D) {
+            out -1 << "\n";
+            continue;
+        }
+
+        ll B = D / coef;
+        if (B <= 0) {
+            out -1 << "\n";
+            continue;
+        }
+
+        ll total = coef * B;
+        out total << "\n";
+    }
+
+    return 0;
+}
