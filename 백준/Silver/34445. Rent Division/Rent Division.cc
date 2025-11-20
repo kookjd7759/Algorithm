fb56350e35b70d9b872ba7fbcff3c9e305ea50f1
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-11-20 14:31:01 +0900

    [Silver V] Title: Rent Division, Time: 12 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/34445. Rent Division/Rent Division.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int N;
+    in N;
+    vector<ll> c(N);
+    ll sumC = 0;
+    Fori(N) {
+        in c[i];
+        sumC += c[i];
+    }
+    ll R;
+    in R;
+
+    ll S2 = sumC + R;
+    ll Nll = (ll)N;
+
+    Fori(N) {
+        ll num = S2 - Nll * c[i];
+        if (num <= -Nll) {
+            out "not possible";
+            return 0;
+        }
+    }
+
+    Fori(N) {
+        ll num = S2 - Nll * c[i];
+        ll p = (num + Nll - 1) / Nll;
+        out p << "\n";
+    }
+
+    return 0;
+}
