7638f2d5e93f4e47167bcb938829c76561433cc7
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-11-23 17:39:17 +0900

    [Bronze II] Title: Треугольник, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/29415. Треугольник/Треугольник.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >>
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    ll S;
+    in S;
+    ll T = 2 * S;
+    ll ans = 0;
+
+    for (ll a = 1; a * a <= T; ++a) {
+        if (T % a != 0) continue;
+        ll b = T / a;
+        if (a > b) continue;
+        ll c2 = a * a + b * b;
+        ll c = sqrtl((long double)c2);
+        if (c * c == c2) ++ans;
+    }
+
+    out ans endl;
+
+    return 0;
+}
