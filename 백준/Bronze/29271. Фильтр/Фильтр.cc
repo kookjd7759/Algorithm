823577cf5405eed67c4cd912e30f94a8092fde08
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-11-23 16:06:03 +0900

    [Bronze II] Title: Фильтр, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29271. Фильтр/Фильтр.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int n;
+    ll r, x;
+    in n >> r >> x;
+    vector<ll> a(n);
+    Fori(n) in a[i];
+
+    ll cur = 0;
+    ll ans = 0;
+
+    Fori(n) {
+        cur += a[i];
+        if (cur > r) cur = r;
+        if (cur <= x) {
+            ans += cur;
+            cur = 0;
+        } else {
+            ans += x;
+            cur -= x;
+        }
+    }
+
+    out ans endl;
+
+    return 0;
+}
