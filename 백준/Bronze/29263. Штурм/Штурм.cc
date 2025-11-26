1d679be03c8b40d48d7378971e6bd0ca53ef416d
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-11-26 13:50:00 +0900

    [Bronze II] Title: Штурм, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Bronze/29263. Штурм/Штурм.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int n, m;
+    in n >> m;
+    vector<vector<int>> a(n, vector<int>(m));
+    Fori(n) Forj(m) in a[i][j];
+
+    int dx[4] = {-1, 1, 0, 0};
+    int dy[4] = {0, 0, -1, 1};
+
+    int cnt = 0;
+    Fori(n) {
+        Forj(m) {
+            bool good = true;
+            for (int d = 0; d < 4; ++d) {
+                int ni = i + dx[d];
+                int nj = j + dy[d];
+                if (0 <= ni && ni < n && 0 <= nj && nj < m) {
+                    if (a[i][j] <= a[ni][nj]) {
+                        good = false;
+                        break;
+                    }
+                }
+            }
+            if (good) ++cnt;
+        }
+    }
+
+    out cnt endl;
+    return 0;
+}
