9014c709b9494cfde36f659239766e60ae17268a
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-11-28 11:57:29 +0900

    [Bronze II] Title: Alternative Blockchain Algorithms, Time: 224 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33247. Alternative Blockchain Algorithms/Alternative Blockchain Algorithms.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; i <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+    int N;
+    in N;
+    int id, pid;
+    ll m;
+    in id >> pid >> m;
+    if (pid != 0) {
+        out "INVALID";
+        return 0;
+    }
+    int prev_id = id;
+    ll bal = m;
+    if (bal < 0) {
+        out "NO_MONEY";
+        return 0;
+    }
+    Fori(N - 1) {
+        in id >> pid >> m;
+        if (pid != prev_id) {
+            out "INVALID";
+            return 0;
+        }
+        prev_id = id;
+        bal += m;
+        if (bal < 0) {
+            out "NO_MONEY";
+            return 0;
+        }
+    }
+    out bal;
+}
