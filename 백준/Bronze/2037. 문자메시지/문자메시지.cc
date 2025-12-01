bbc7f2b723aaabb1fbeb791ecfe7290f43ccf949
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-12-01 14:21:54 +0900

    [Bronze I] Title: 문자메시지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2037. 문자메시지/문자메시지.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int eng[26]{ 1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,4,1,2,3,1,2,3,4 };
+    int type[26]{ 2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,7,8,8,8,9,9,9,9 };
+    int p, w; in p >> w;
+    int prev_type(0), time(0);
+    string line; getline(cin, line); getline(cin, line);
+    Fori(line.size()) {
+        char c = line[i];
+        if (c == ' ') {
+            time += p;
+            prev_type = 1;
+            continue;
+        }
+        if (prev_type == type[c - 'A']) time += w;
+        time += p * eng[c - 'A'];
+        prev_type = type[c - 'A'];
+    }
+    out time;
+}
