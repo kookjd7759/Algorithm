3cd1ba6f29d6773e9679e40493aaf59a5a3c90ae
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-12-01 11:02:49 +0900

    [Bronze II] Title: Игра, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/22108. Игра/Игра.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+    int T;
+    in T;
+    while (T--) {
+        int n;
+        in n;
+        vector<vector<int>> a(n, vector<int>(n));
+        bool good = false;
+        Fori(n) {
+            Forj(n) {
+                in a[i][j];
+                if (a[i][j] == 0) good = true;
+            }
+        }
+        Fori(n) {
+            Forj(n) {
+                if (i + 1 < n && a[i][j] == a[i + 1][j]) good = true;
+                if (j + 1 < n && a[i][j] == a[i][j + 1]) good = true;
+            }
+        }
+        if (good) out "YES\n";
+        else out "NO\n";
+    }
+}
