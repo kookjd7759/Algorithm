def2a8cfb8e1be4ed391f977b4ae73f7201ad221
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-12-01 10:55:02 +0900

    [Bronze II] Title: Hungry Wolves, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/33534. Hungry Wolves/Hungry Wolves.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+    long double f;
+    in f;
+    long double circ = 2.0L * sqrt(f * acosl(-1.0L));
+    long double need = ceill(circ * 10.0L) / 10.0L;
+    Fixed(1);
+    out (double)need;
+}
