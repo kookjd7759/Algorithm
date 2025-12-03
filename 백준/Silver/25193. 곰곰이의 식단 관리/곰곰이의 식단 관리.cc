cd0ef53f982385f0141fdbacd46f0b9665d38cab
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-12-03 10:17:04 +0900

    [Silver V] Title: 곰곰이의 식단 관리, Time: 0 ms, Memory: 2392 KB -BaekjoonHub

---
+++ 백준/Silver/25193. 곰곰이의 식단 관리/곰곰이의 식단 관리.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int n, noC(0); in n;
+    string st; in st;
+    Fori(st.size()) if (st[i] != 'C') noC++;
+    if (noC == 0) out n;
+    else {
+        out n / (noC + 1);
+    }
+}
