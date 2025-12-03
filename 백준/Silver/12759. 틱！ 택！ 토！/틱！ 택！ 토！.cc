1f8cb240ad33d26dec81950ac3563a90bbb343f3
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-12-03 10:28:06 +0900

    [Silver V] Title: 틱! 택! 토!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/12759. 틱！ 택！ 토！/틱！ 택！ 토！.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+    int start;
+    in start;
+
+    int board[3][3] = {0};
+    int cur = start;
+
+    auto check = [&](int p) {
+        Fori(3) {
+            if (board[i][0] == p && board[i][1] == p && board[i][2] == p) return true;
+            if (board[0][i] == p && board[1][i] == p && board[2][i] == p) return true;
+        }
+        if (board[0][0] == p && board[1][1] == p && board[2][2] == p) return true;
+        if (board[0][2] == p && board[1][1] == p && board[2][0] == p) return true;
+        return false;
+    };
+
+    int winner = 0;
+    Fori(9) {
+        int r, c;
+        in r >> c;
+        if (winner == 0) {
+            board[r - 1][c - 1] = cur;
+            if (check(cur)) winner = cur;
+            cur = 3 - cur;
+        }
+    }
+
+    out winner;
+}
