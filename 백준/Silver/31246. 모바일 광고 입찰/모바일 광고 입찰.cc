f8631deecf13d940f26112379624c118733db137
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-12-04 12:16:45 +0900

    [Silver V] Title: 모바일 광고 입찰, Time: 28 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/31246. 모바일 광고 입찰/모바일 광고 입찰.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    ll N, K;
+    in N >> K;
+    vector<ll> req(N);
+    Fori((int)N) {
+        ll A, B;
+        in A >> B;
+        ll t = B - A;
+        if (t < 0) t = 0;
+        req[i] = t;
+    }
+    sort(req.begin(), req.end());
+    ll ans = req[K - 1];
+    out ans;
+
+    return 0;
+}
