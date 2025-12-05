91eacdafe59f21a1cf415df29952f4f4b37c6377
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-12-05 13:54:49 +0900

    [Silver V] Title: Potato, Time: 28 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/28464. Potato/Potato.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int N; in N;
+
+    vector<int> a(N);
+    Fori(N) in a[i];
+
+    sort(a.begin(), a.end(), greater<int>());
+
+    ll total = 0;
+    Fori(N) total += a[i];
+
+    int cntP = (N + 1) / 2;
+    ll park = 0;
+    Fori(cntP) park += a[i];
+
+    ll seong = total - park;
+
+    out seong spc park;
+
+    return 0;
+}
