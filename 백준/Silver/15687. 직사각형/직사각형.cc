5c954bb7cc7d0e5136b225352f0dcfbcb31bde02
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-12-05 10:57:21 +0900

    [Silver V] Title: 직사각형, Time: 8 ms, Memory: 4452 KB -BaekjoonHub

---
+++ 백준/Silver/15687. 직사각형/직사각형.cc
@@ -0,0 +1,18 @@
+class Rectangle {
+private:
+    int width, height;
+
+public :
+    Rectangle(int width, int height): width(width), height(height) {}
+    int get_width() const { return width; }
+    int get_height() const { return height; }
+    void set_width(int width) {
+        if (0 < width && width <= 1000) this->width = width;
+    }
+    void set_height(int height) {
+        if (0 < height && height <= 2000) this->height = height;
+    }
+    int area() const { return width * height; }
+    int perimeter() const { return width * 2 + height * 2; }
+    bool is_square() const { return width == height; }
+};
