21ed3778dc0f6aa4f99b08ffe7188a489614dcd3
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-12-07 16:57:31 +0900

    [Silver V] Title: 공포의 면담실, Time: 68 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/30088. 공포의 면담실/공포의 면담실.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int N; in N;
+
+    vector<ll> dept(N);
+    Fori(N) {
+        int k; in k;
+        ll sum = 0;
+        Forj(k) {
+            int t;
+            in t;
+            sum += t;
+        }
+        dept[i] = sum;
+    }
+
+    sort(dept.begin(), dept.end());
+
+    ll cur = 0, ans = 0;
+    Fori(N) {
+        cur += dept[i];
+        ans += cur;
+    }
+
+    out ans;
+
+    return 0;
+}
