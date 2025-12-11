ed4196a0c49176142cd4ed6dd18d058ebe733618
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-12-11 19:14:32 +0900

    [Bronze III] Title: M, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34917. M/M.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+    
+    int tc; in tc;
+    while (tc--) {
+        int n; in n;
+        auto basic = [&]() -> void {
+            out '#';
+            Fori(n - 2) out '.';
+            out '#' endl;
+            };
+        int idx(0);
+        Fori(n / 2 + 1) {
+            Forj(n) {
+                if (j == idx || (n - j - 1) == idx) {
+                    out '#';
+                    continue;
+                }
+                if (j == 0 || j == (n - 1)) out '#';
+                else out '.';
+            }
+            ent;
+            idx++;
+        }
+        Fori(n / 2) basic();
+    }
+}
