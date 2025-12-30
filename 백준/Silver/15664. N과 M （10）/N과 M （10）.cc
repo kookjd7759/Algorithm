cfeb082a9663587fbca349517d43fc0898decd73
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-12-30 12:17:04 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+
+using namespace std;
+
+int N, M;
+vector<int> arr;
+vector<int> ans;
+set<vector<int>> s;
+
+void dfs(int start, int prev, int depth) {
+	if (depth == M) {
+		if (s.count(ans)) return;
+		for (int i = 0; i < M; ++i)
+			cout << ans[i] << ' ';
+		cout << "\n";
+		s.insert(ans);
+		return;
+	}
+	for (int i = start; i < N; ++i) {
+		if (arr[i] < prev) continue;
+		ans[depth] = arr[i];
+		dfs(i + 1, arr[i], depth + 1);
+	}
+}
+
+int main() {
+	Sync;
+
+	cin >> N >> M;
+	arr.resize(N); ans.resize(M);
+	for (int i = 0; i < N; ++i) cin >> arr[i];
+	sort(arr.begin(), arr.end());
+
+	dfs(0, 0, 0);
+
+	return 0;
+}
