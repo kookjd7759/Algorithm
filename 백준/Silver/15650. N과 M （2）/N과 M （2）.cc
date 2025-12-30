480083d9da55c83f18fce30edc4b1266f20cf93d
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-12-30 11:51:51 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,43 +1,30 @@
 #include <iostream>
-#include <array>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
 
 using namespace std;
 
 int N, M;
+int arr[10];
 
-void PrintArr(const array<int, 8>& Arr) {
-    for (int i = 0; i < M; i++)
-        cout << Arr.at(i) << " ";
-    cout << "\n";
-}
-
-bool Comp(array<int, 8> Arr, int index) {
-    for (int i = 0; i < index; i++) {
-        if (Arr.at(i) == Arr.at(index) || Arr.at(i) > Arr.at(index))
-            return false;
-    }
-
-    return true;
-}
-
-void Func(array<int, 8>& Arr, int index) {
-    if (index == M) {
-        PrintArr(Arr);
-        return;
-    }
-
-    for (int i = 0; i < N; i++) {
-        Arr.at(index) = i + 1;
-        if (Comp(Arr, index)) 
-            Func(Arr, index + 1);
-    }
+void dfs(int start, int depth) {
+	if (depth == M) {
+		for (int i = 0; i < M; ++i)
+			cout << arr[i] << ' ';
+		cout << "\n";
+		return;
+	}
+	for (int i = start; i <= N; ++i) {
+		arr[depth] = i;
+		dfs(i + 1, depth + 1);
+	}
 }
 
 int main() {
-    cin >> N >> M;
-    array<int, 8> Arr{};
+	Sync;
 
-    Func(Arr, 0);
+	cin >> N >> M;
+	dfs(1, 0);
 
-    return 0;
+	return 0;
 }
