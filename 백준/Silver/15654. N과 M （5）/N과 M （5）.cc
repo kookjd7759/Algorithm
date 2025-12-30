18c6b0c06c3b13e79dab7d8565e181b3baf91e5b
Donggyun Kook <67672017+kookjd7759@users.noreply.github.com>
2025-12-30 12:00:29 +0900

    [Silver III] Title: N과 M (5), Time: 20 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -2,46 +2,41 @@
 #include <algorithm>
 #include <vector>
 
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+
 using namespace std;
 
-int comp(const void* a, const void* b) { return *(int*)a - *(int*)b; }
+int N, M;
+vector<int> arr;
+vector<int> ans;
+vector<bool> visited;
 
-int list[8], visited[8];
-vector<int> vec;
-
-void func(int size) {
-	if (size == 1) {
-		for (int i = 0; i < 8; i++) {
-			if (!visited[i]) {
-				for (const auto iter : vec)
-					cout << iter << " ";
-				cout << list[i];
-				cout << "\n";
-			}
-		}
+void dfs(int depth) {
+	if (depth == M) {
+		for (int i = 0; i < M; ++i)
+			cout << ans[i] << ' ';
+		cout << "\n";
+		return;
 	}
-	else {
-		for (int i = 0; i < 8; i++) {
-			if (!visited[i]) {
-				visited[i] = true;
-				vec.push_back(list[i]);
-				func(size - 1);
-				vec.pop_back();
-				visited[i] = false;
-			}
-		}
+	for (int i = 0; i < N; ++i) {
+		if (visited[i]) continue;
+		visited[i] = true;
+		ans[depth] = arr[i];
+		dfs(depth + 1);
+		visited[i] = false;
 	}
 }
 
 int main() {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	Sync;
 
-	int n, m; cin >> n >> m;
-	for (int i = n; i < 8; i++) visited[i] = true;
-	for (int i = 0; i < n; i++) {
-		cin >> list[i];
-		visited[i] = false;
-	}
-	qsort(list, n, sizeof(*list), comp);
-	func(m);
+	cin >> N >> M;
+	arr.resize(N); ans.resize(M);
+	visited.assign(N, false);
+	for (int i = 0; i < N; ++i) cin >> arr[i];
+	sort(arr.begin(), arr.end());
+
+	dfs(0);
+
+	return 0;
 }
