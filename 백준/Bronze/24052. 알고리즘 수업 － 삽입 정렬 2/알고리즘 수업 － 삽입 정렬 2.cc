8583945bfa423caec5cda11a0ade879f7f2dc3d9
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-02-18 16:08:23 +0900

    [Bronze I] Title: 알고리즘 수업 - 삽입 정렬 2, Time: 80 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Bronze/24052. 알고리즘 수업 － 삽입 정렬 2/알고리즘 수업 － 삽입 정렬 2.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int n; ll K; in n >> K;
+    vector<ll> a(n + 1);
+    For1i(n) in a[i];
+
+    ll cnt = 0;
+
+    for (int i = 2; i <= n; ++i) {
+        ll newItem = a[i];
+        int loc = i - 1;
+
+        while (loc >= 1 && newItem < a[loc]) {
+            a[loc + 1] = a[loc];
+            if (++cnt == K) {
+                For1i(n) out a[i] << (i == n ? '\n' : ' ');
+                return 0;
+            }
+            --loc;
+        }
+
+        if (loc + 1 != i) {
+            a[loc + 1] = newItem;
+            if (++cnt == K) {
+                For1i(n) out a[i] << (i == n ? '\n' : ' ');
+                return 0;
+            }
+        }
+    }
+
+    out -1 endl;
+
+    return 0;
+}
