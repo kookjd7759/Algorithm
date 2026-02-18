f9e4c1cbad5902b3ae7cc7e459b0f1dcb3308a42
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-02-18 15:28:05 +0900

    [Bronze I] Title: 문자열 압축 해제, Time: 0 ms, Memory: 4116 KB -BaekjoonHub

---
+++ 백준/Bronze/23746. 문자열 압축 해제/문자열 압축 해제.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int n; in n;
+    vector<string> mp(26);
+    Fori(n) {
+        string p; char c;
+        in p >> c;
+        mp[c - 'A'] = p;
+    }
+
+    string comp; in comp;
+    ll S, E; in S >> E;
+
+    ll tot = 0;
+    Fori((int)comp.size()) tot += (ll)mp[comp[i] - 'A'].size();
+
+    string full; full.reserve((size_t)tot);
+    Fori((int)comp.size()) full += mp[comp[i] - 'A'];
+
+    out full.substr((size_t)(S - 1), (size_t)(E - S + 1)) endl;
+
+    return 0;
+}
