b50bdc593b3d02d3d1a13f3f1c5e1b9990bcc784
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-02-18 15:28:48 +0900

    [Bronze I] Title: 등차수열의 합, Time: 24 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Bronze/28115. 등차수열의 합/등차수열의 합.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int n; in n;
+    vector<ll> a(n);
+    Fori(n) in a[i];
+
+    if (n >= 3) {
+        ll d = a[1] - a[0];
+        for (int i = 2; i < n; ++i) if (a[i] - a[i - 1] != d) { out "NO" endl; return 0; }
+    }
+
+    out "YES" endl;
+    Fori(n) out a[i] << (i + 1 == n ? '\n' : ' ');
+    Fori(n) out 0 << (i + 1 == n ? '\n' : ' ');
+
+    return 0;
+}
