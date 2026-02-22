6c8dac6186e506c49881e80e0d930d81cab7fe28
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-02-22 20:20:32 +0900

    [Bronze III] Title: SUAPC 2025 Summer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/35290. SUAPC 2025 Summer/SUAPC 2025 Summer.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >>
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+    int N; in N;
+    vector<int> ok(11, 0); ok[2]=ok[6]=ok[7]=ok[8]=1;
+    out (ok[N] ? "Think before submission" : "Solve harder problems") endl;
+    return 0;
+}
