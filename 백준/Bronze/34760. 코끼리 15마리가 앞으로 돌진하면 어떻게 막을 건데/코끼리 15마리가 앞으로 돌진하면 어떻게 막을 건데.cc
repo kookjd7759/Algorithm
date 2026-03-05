6663899ade1d1d3c4d9a47d396e8cca8eb9271c2
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-05 20:45:02 +0900

    [Bronze I] Title: 코끼리 15마리가 앞으로 돌진하면 어떻게 막을 건데, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34760. 코끼리 15마리가 앞으로 돌진하면 어떻게 막을 건데/코끼리 15마리가 앞으로 돌진하면 어떻게 막을 건데.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    vector<ll> a(15); Fori(15) in a[i];
+    ll mAll=0,m14=0; Fori(15) mAll=max(mAll,a[i]); Fori(14) m14=max(m14,a[i]);
+    ll ans = (a[14]>m14 ? a[14] : mAll+1);
+    out ans;
+    return 0;
+}
