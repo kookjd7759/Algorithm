78160544cee138b3f047da52d6f29cd1dc6c5f60
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-10 14:06:40 +0900

    [Bronze I] Title: Ready for Contest, Time: 44 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Bronze/32316. Ready for Contest/Ready for Contest.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int n, m; in n >> m;
+    vector<int> chk(n + 1, 0);
+    Fori(m) {
+        int p, l; in p >> l;
+        if (l == 1) chk[p] |= 1;
+        else if (l == 2) chk[p] |= 2;
+    }
+    For1i(n) if (chk[i] == 3) out i endl;
+
+    return 0;
+}
