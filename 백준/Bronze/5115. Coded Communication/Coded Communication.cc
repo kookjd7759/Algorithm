64272ba730c136e3a342679da98ec93cd3892b3f
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-11 19:48:18 +0900

    [Bronze I] Title: Coded Communication, Time: 4 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Bronze/5115. Coded Communication/Coded Communication.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int K; in K;
+    For1i(K) {
+        int n, b; in n >> b;
+        vector<string> v(n);
+        Forj(n) in v[j];
+        string r; in r;
+
+        int ans = b;
+        Forj(n) {
+            int cur = 0;
+            Fork(b) if(v[j][k] != r[k]) ++cur;
+            ans = min(ans, cur);
+        }
+
+        out "Data Set " << i << ":" endl;
+        out ans endl;
+        ent;
+    }
+
+    return 0;
+}
