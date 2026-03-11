54db7a5999a4531c2574eef0e21325af3e3fc01c
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-11 11:18:12 +0900

    [Silver V] Title: Name Generation, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/25268. Name Generation/Name Generation.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int N; in N;
+    string c = "bcdfghjklmnpqrstvwxyz", v = "aeiou";
+
+    for(char a : c) {
+        for(char b : v) {
+            for(char d : c) {
+                for(char e : v) {
+                    for(char f : c) {
+                        string s = "";
+                        s += a; s += b; s += d; s += e; s += f;
+                        out s endl;
+                        if(--N == 0) return 0;
+                    }
+                }
+            }
+        }
+    }
+
+    return 0;
+}
