dd4cba32a8015635df025306050689bc196c9843
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-12 13:46:41 +0900

    [Bronze I] Title: Ребус, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/19802. Ребус/Ребус.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    string s; getline(cin, s);
+    string ans = "";
+    int n = s.size(), i = 0;
+
+    while (i < n) {
+        while (i < n && s[i] == ' ') i++;
+        if (i >= n) break;
+
+        int l = 0, r = 0;
+        while (i < n && s[i] == '\'') l++, i++;
+
+        string w = "";
+        while (i < n && s[i] >= 'a' && s[i] <= 'z') w += s[i], i++;
+
+        while (i < n && s[i] == '\'') r++, i++;
+
+        ans += w.substr(l, (int)w.size() - l - r);
+    }
+
+    out ans << "\n";
+
+    return 0;
+}
