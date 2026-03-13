6e8d05316534c50e8f0e35e7fe293ea5938a0997
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-13 12:28:10 +0900

    [Bronze I] Title: General Knight, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Bronze/20574. General Knight/General Knight.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >>
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int a, b; in a >> b;
+    string s; in s;
+    int x = s[0] - 'a', y = s[1] - '1';
+
+    set<pair<int,int>> st;
+    int dx[8] = {a, a, -a, -a, b, b, -b, -b};
+    int dy[8] = {b, -b, b, -b, a, -a, a, -a};
+
+    Fori(8) {
+        int nx = x + dx[i], ny = y + dy[i];
+        if (0 <= nx && nx < 8 && 0 <= ny && ny < 8) st.insert({nx, ny});
+    }
+
+    out (int)st.size() endl;
+    bool first = 1;
+    for (auto [cx, cy] : st) {
+        if (!first) out " ";
+        first = 0;
+        out char('a' + cx) << char('1' + cy);
+    }
+    ent;
+
+    return 0;
+}
