42678505481371ed20200d95f25f283290d8bc41
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-13 09:57:17 +0900

    [Bronze I] Title: Longest Arithmetic, Time: 236 ms, Memory: 3704 KB, Score: 11 point -BaekjoonHub

---
+++ 백준/Bronze/23913. Longest Arithmetic/Longest Arithmetic.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >>
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int T; in T;
+    For1i(T) {
+        int N; in N;
+        vector<ll> a(N);
+        Forj(N) in a[j];
+
+        int ans = 2, cur = 2;
+        ll d = a[1] - a[0];
+        for (int j = 2; j < N; ++j) {
+            if (a[j] - a[j - 1] == d) cur++;
+            else d = a[j] - a[j - 1], cur = 2;
+            ans = max(ans, cur);
+        }
+
+        out "Case #" << i << ": " << ans endl;
+    }
+
+    return 0;
+}
