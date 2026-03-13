0cf6380046c17ec28fcb78e7626e1db85eec348c
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-13 12:13:41 +0900

    [Bronze I] Title: Preludes, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/23207. Preludes/Preludes.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >>
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    map<string, string> mp;
+    mp["A#"] = "Bb";
+    mp["Bb"] = "A#";
+    mp["C#"] = "Db";
+    mp["Db"] = "C#";
+    mp["D#"] = "Eb";
+    mp["Eb"] = "D#";
+    mp["F#"] = "Gb";
+    mp["Gb"] = "F#";
+    mp["G#"] = "Ab";
+    mp["Ab"] = "G#";
+
+    string note, tonality;
+    int tc = 1;
+    while (in note >> tonality) {
+        out "Case " << tc++ << ": ";
+        if (mp.count(note)) out mp[note] spc tonality endl;
+        else out "UNIQUE" endl;
+    }
+
+    return 0;
+}
