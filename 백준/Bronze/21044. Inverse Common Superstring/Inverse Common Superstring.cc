8ffb5c171010c605638851b20879570b3dc786f8
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-17 08:59:24 +0900

    [Bronze I] Title: Inverse Common Superstring, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/21044. Inverse Common Superstring/Inverse Common Superstring.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    string s; in s;
+    int cur = 0, mx = 0;
+    for (char c : s) {
+        if (c == 'a') ++cur, mx = max(mx, cur);
+        else cur = 0;
+    }
+    Fori(mx + 1) out 'a';
+    ent;
+
+    return 0;
+}
