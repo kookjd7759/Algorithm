76e35fe5c98d5889e88f9e7fa913158e0d6aafad
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-16 15:34:42 +0900

    [Bronze I] Title: Mars Message, Time: 20 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Bronze/21324. Mars Message/Mars Message.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; i <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int N; in N;
+    string ans;
+    for (int i = 0; i < N; i += 2) {
+        long double a, b; in a >> b;
+        int x = (int)(a / 22.5L), y = (int)(b / 22.5L);
+        ans += char(x * 16 + y);
+    }
+    out ans endl;
+
+    return 0;
+}
