7393ce6291f4776abe5c3a9364d0abbdc3b89101
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-17 09:38:07 +0900

    [Bronze I] Title: Game, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21623. Game/Game.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    ll k, n; in k >> n;
+    ll cur = n, rounds = 0, x;
+    Fori(k) {
+        in x;
+        if (cur - x >= 0) cur -= x;
+        if (cur == 0) rounds++, cur = n;
+    }
+    if (cur == n && rounds > 0 && cur == n) cur = 0;
+    out rounds endl;
+    out cur endl;
+
+    return 0;
+}
