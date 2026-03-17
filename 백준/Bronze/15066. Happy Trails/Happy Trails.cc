4c4a9bd7a6f78465f5b40b9ce693cf575d3250bc
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-17 10:53:50 +0900

    [Bronze I] Title: Happy Trails, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Bronze/15066. Happy Trails/Happy Trails.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+    Fixed(2);
+
+    int n; in n;
+    long double ans = 0.0L, pi = acosl(-1.0L);
+    Fori(n) {
+        long double a, d; in a >> d;
+        ans += d * sinl(a * pi / 180.0L);
+    }
+    out fabsl(ans) endl;
+
+    return 0;
+}
