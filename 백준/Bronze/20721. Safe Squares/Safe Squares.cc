16d297f7fb71297d13190f97f94b4e5e5a4a95ef
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-17 14:28:22 +0900

    [Bronze I] Title: Safe Squares, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/20721. Safe Squares/Safe Squares.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    vector<string> a(8);
+    Fori(8) in a[i];
+    bool r[8] = {}, c[8] = {};
+    Fori(8) Forj(8) if (a[i][j] == 'R') r[i] = true, c[j] = true;
+
+    int ans = 0;
+    Fori(8) Forj(8) if (!r[i] && !c[j]) ++ans;
+    out ans endl;
+
+    return 0;
+}
