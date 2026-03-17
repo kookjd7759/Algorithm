8cb124799c7de46d471156552f6eca0a83b5671a
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-17 14:18:27 +0900

    [Bronze I] Title: Reading Digits, Time: 4 ms, Memory: 3344 KB -BaekjoonHub

---
+++ 백준/Bronze/13858. Reading Digits/Reading Digits.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int k, pos; in k >> pos;
+    string s; in s;
+
+    while (k--) {
+        string t;
+        t.reserve(s.size() * 5);
+        for (int i = 0; i + 1 < (int)s.size(); i += 2) {
+            int cnt = s[i] - '0';
+            t.append(cnt, s[i + 1]);
+        }
+        s = t;
+    }
+
+    out s[pos] endl;
+
+    return 0;
+}
