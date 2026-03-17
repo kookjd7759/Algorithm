7da46d1d72a64779ead4b93eea3ebd5534ace3ed
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-17 10:25:56 +0900

    [Bronze I] Title: Rectangle and Squares, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13035. Rectangle and Squares/Rectangle and Squares.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int t; in t;
+    while (t--) {
+        ll A, B, C; in A >> B >> C;
+        ll x = A * B, s = C * C;
+        ll q = x / s, a = max(1LL, q) * s, b = (q + 1) * s;
+        if (q == 0) out b endl;
+        else out (x - a <= b - x ? a : b) endl;
+    }
+
+    return 0;
+}
