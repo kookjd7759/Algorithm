0d7f0e858cbbbf0df95bfdb54eeffbcd1dbba108
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-18 09:47:21 +0900

    [Bronze I] Title: Exam, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/26563. Exam/Exam.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int T; in T;
+    while (T--) {
+        int k; in k;
+        string a, b; in a >> b;
+        int same = 0, diff = 0, n = a.size();
+        Fori(n) if (a[i] == b[i]) ++same; else ++diff;
+        out min(same, k) + min(diff, n - k) endl;
+    }
+
+    return 0;
+}
