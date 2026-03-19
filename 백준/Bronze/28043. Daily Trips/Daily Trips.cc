76d3fb53f41e18b55d4e9e28a28de783617f7529
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-19 12:31:23 +0900

    [Bronze I] Title: Daily Trips, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28043. Daily Trips/Daily Trips.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int N, H, W; in N >> H >> W;
+    Fori(N) {
+        char a, b; in a >> b;
+        char x, y;
+
+        if (a == 'Y' || W == 0) x = 'Y', --H, ++W;
+        else x = 'N';
+
+        if (b == 'Y' || H == 0) y = 'Y', --W, ++H;
+        else y = 'N';
+
+        out x spc y endl;
+    }
+
+    return 0;
+}
