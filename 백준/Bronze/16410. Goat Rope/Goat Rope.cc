48e4e636eafa2e23dc9d824460a44549ed838f23
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-19 12:36:18 +0900

    [Bronze I] Title: Goat Rope, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/16410. Goat Rope/Goat Rope.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+    Fixed(10);
+
+    double x, y, x1, y1, x2, y2; in x >> y >> x1 >> y1 >> x2 >> y2;
+    double cx = max(x1, min(x, x2)), cy = max(y1, min(y, y2));
+    double dx = x - cx, dy = y - cy;
+    out sqrt(dx * dx + dy * dy) endl;
+
+    return 0;
+}
