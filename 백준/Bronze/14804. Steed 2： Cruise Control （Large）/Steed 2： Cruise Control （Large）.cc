6fd143b78ed3abac82e5aa83da903ce62c663c86
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-19 14:12:23 +0900

    [Bronze I] Title: Steed 2: Cruise Control (Large), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14804. Steed 2： Cruise Control （Large）/Steed 2： Cruise Control （Large）.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+    Fixed(6);
+
+    int T; in T;
+    For1i(T) {
+        double D; int N; in D >> N;
+        double mx = 0;
+        Forj(N) {
+            double K, S; in K >> S;
+            mx = max(mx, (D - K) / S);
+        }
+        out "Case #" << i << ": " << D / mx endl;
+    }
+
+    return 0;
+}
