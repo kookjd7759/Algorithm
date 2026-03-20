7004da084a6d31eb94ba5c5e0ad53da2abe130ac
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-20 17:41:52 +0900

    [Bronze I] Title: Spaceship, Time: 20 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Bronze/16244. Spaceship/Spaceship.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int n; in n;
+    vector<ll> a(n);
+    ll sum = 0;
+    Fori(n) in a[i], sum += a[i];
+
+    int idx = -1;
+    Fori(n) if (a[i] * 2 == sum) { idx = i; break; }
+
+    Fori(n) if (i != idx) out a[i] << " ";
+    out a[idx] endl;
+
+    return 0;
+}
