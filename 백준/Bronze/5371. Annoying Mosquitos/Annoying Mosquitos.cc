390319362787d36a5adfc2c57db0e4f36a31a0a0
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-23 10:25:43 +0900

    [Bronze I] Title: Annoying Mosquitos, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5371. Annoying Mosquitos/Annoying Mosquitos.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int T; in T;
+    while (T--) {
+        int n; in n;
+        vector<pair<int, int>> a(n);
+        Fori(n) in a[i].first >> a[i].second;
+        int m, ans = 0; in m;
+        vector<int> hit(n);
+        Fori(m) {
+            int x, y; in x >> y;
+            Forj(n) if (!hit[j] && abs(a[j].first - x) <= 50 && abs(a[j].second - y) <= 50) hit[j] = 1, ++ans;
+        }
+        out ans endl;
+    }
+
+    return 0;
+}
