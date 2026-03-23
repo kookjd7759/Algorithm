ada65e3121b4ffbb6b71efd9df98cf59a0d39e1a
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-23 10:40:33 +0900

    [Bronze I] Title: Moist (Small2), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/12086. Moist （Small2）/Moist （Small2）.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int T; in T;
+    cin.ignore();
+    For1i(T) {
+        int N, ans = 0; in N;
+        cin.ignore();
+        string mx = "";
+        Forj(N) {
+            string s; getline(cin, s);
+            if (j && s < mx) ++ans;
+            else mx = s;
+            if (s > mx) mx = s;
+        }
+        out "Case #" << i << ": " << ans endl;
+    }
+
+    return 0;
+}
