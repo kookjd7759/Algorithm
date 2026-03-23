59a1fa4d8b52c419c78bdcabfabf8db80fafdec4
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-23 11:39:30 +0900

    [Bronze I] Title: Grades, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14194. Grades/Grades.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int T; in T;
+    while (T--) {
+        ll n, x, mn = 101, mx = -1, sum = 0; in n;
+        Fori(n) in x, mn = min(mn, x), mx = max(mx, x), sum += x;
+        ll diff = llabs(n * (mn + mx) - 2 * sum);
+        if (diff < 2 * n) out "Yes" endl;
+        else out "No" endl;
+    }
+
+    return 0;
+}
