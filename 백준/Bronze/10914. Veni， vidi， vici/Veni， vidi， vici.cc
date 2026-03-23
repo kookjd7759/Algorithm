daa58132acf5e5035d30352d8ea2a8c25d139819
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-23 11:22:15 +0900

    [Bronze I] Title: Veni, vidi, vici, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10914. Veni， vidi， vici/Veni， vidi， vici.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int n; in n;
+    string s;
+    bool first = 1;
+    while (in s) {
+        if (!first) out " ";
+        first = 0;
+        for (int i = 0; i + 1 < (int)s.size(); i += 2) {
+            int y = s[i] - 'a', z = s[i + 1] - 'a', x = (y + z - n) % 26;
+            if (x < 0) x += 26;
+            out char('a' + x);
+        }
+    }
+    ent;
+
+    return 0;
+}
