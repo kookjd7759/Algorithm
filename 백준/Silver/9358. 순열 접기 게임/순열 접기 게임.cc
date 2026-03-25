d3e1c048c5d5b114b377fd925a5044915e006ef5
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-25 21:08:52 +0900

    [Silver V] Title: 순열 접기 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9358. 순열 접기 게임/순열 접기 게임.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int T; in T;
+    For1i(T) {
+        int N; in N;
+        vector<ll> a(N);
+        Forj(N) in a[j];
+
+        while ((int)a.size() > 2) {
+            int n = a.size(), m = (n + 1) / 2;
+            vector<ll> b(m);
+            Forj(m) b[j] = a[j] + a[n - 1 - j];
+            a = b;
+        }
+
+        out "Case #" << i << ": " << (a[0] > a[1] ? "Alice" : "Bob") endl;
+    }
+
+    return 0;
+}
