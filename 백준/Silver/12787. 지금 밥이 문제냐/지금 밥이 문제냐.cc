41dd6b468aa060f3e81be115027e89bb2fa14493
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-26 09:06:28 +0900

    [Silver V] Title: 지금 밥이 문제냐, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/12787. 지금 밥이 문제냐/지금 밥이 문제냐.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define ull unsigned long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+
+    int T; in T;
+    while (T--) {
+        int M; string N; in M >> N;
+        if (M == 1) {
+            ull x = 0;
+            string cur = "";
+            for (char c : N) {
+                if (c == '.') x = (x << 8) + stoull(cur), cur = "";
+                else cur += c;
+            }
+            x = (x << 8) + stoull(cur);
+            out x endl;
+        } else {
+            ull x = stoull(N);
+            vector<ull> a(8);
+            for (int i = 7; i >= 0; --i) a[i] = x & 255, x >>= 8;
+            Fori(8) {
+                if (i) out ".";
+                out a[i];
+            }
+            ent;
+        }
+    }
+
+    return 0;
+}
