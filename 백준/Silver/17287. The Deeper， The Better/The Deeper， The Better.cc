69c8e47c0fb72c3b05e6497c4d7c29a12fb2d03d
국동균(Kook Donggyun) <67672017+kookjd7759@users.noreply.github.com>
2026-03-29 23:51:07 +0900

    [Silver V] Title: The Deeper, The Better, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/17287. The Deeper， The Better/The Deeper， The Better.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define Sync ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define Fixed(x) cout << fixed; cout.precision(x)
+#define Interactive cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define spc << " " <<
+#define endl << "\n"
+#define ent cout << "\n"
+#define out cout <<
+#define in cin >> 
+#define Fori(x) for (int i = 0; i < x; ++i)
+#define Forj(x) for (int j = 0; j < x; ++j)
+#define Fork(x) for (int k = 0; k < x; ++k)
+#define For1i(x) for (int i = 1; i <= x; ++i)
+#define For1j(x) for (int j = 1; j <= x; ++j)
+#define For1k(x) for (int k = 1; k <= x; ++k)
+
+using namespace std;
+
+int main() {
+    Sync;
+    string s; in s;
+    int cur = 0, ans = 0;
+    for (char c : s) {
+        if (c == '(') cur += 1;
+        else if (c == ')') cur -= 1;
+        else if (c == '{') cur += 2;
+        else if (c == '}') cur -= 2;
+        else if (c == '[') cur += 3;
+        else if (c == ']') cur -= 3;
+        else ans = max(ans, cur);
+    }
+    out ans;
+    return 0;
+}
